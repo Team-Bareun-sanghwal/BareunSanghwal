355a70e7a11e3fb5e3fdc30baec024bd63ddffae
전영빈 <binkorea2@gmail.com>
2024-04-16 15:44:13 +0900

    feat: Member Exception Handler 추가

--- backend/src/main/java/com/a106/rightlife/global/commom/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/a106/rightlife/global/commom/exception/GlobalExceptionHandler.java
@@ -1,10 +1,19 @@
 package com.a106.rightlife.global.commom.exception;
 
+import com.a106.rightlife.global.commom.response.BaseResponse;
+import com.a106.rightlife.member.exception.MemberException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MemberException.class)
+    public ResponseEntity<?> memberExceptionHandler(MemberException e) {
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+            .body(BaseResponse.error(e.getErrorCode().getStatus().value(), e.getMessage()));
+    }
 }
