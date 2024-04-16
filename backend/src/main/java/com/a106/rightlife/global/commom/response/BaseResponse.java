b29e7d4c8a9320b5d7bab9281e6d10a3433125d1
전영빈 <binkorea2@gmail.com>
2024-04-16 15:39:25 +0900

    feat: Base API Response 생성

---
+++ backend/src/main/java/com/a106/rightlife/global/commom/response/BaseResponse.java
@@ -0,0 +1,22 @@
+package com.a106.rightlife.global.commom.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseResponse<T> {
+
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> BaseResponse<T> success(int status, String message, T data) {
+        return new BaseResponse<>(status, message, data);
+    }
+
+    public static BaseResponse<Void> error(int status, String message) {
+        return new BaseResponse<>(status, message, null);
+    }
+
+}
