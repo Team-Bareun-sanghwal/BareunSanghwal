17241011b74a254940800ef7a0a8edf105e4d66d
전영빈 <binkorea2@gmail.com>
2024-04-16 15:40:37 +0900

    feat: ControllerAdvice 생성

---
+++ backend/src/main/java/com/a106/rightlife/global/commom/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.a106.rightlife.global.commom.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
