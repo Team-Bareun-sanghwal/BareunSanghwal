0ca0e6405a3d9805183108fc8984154df33e2c02
신종욱 <rmm1@naver.com>
2024-04-18 08:51:04 +0900

    Revert "Merge branch 'BE/member/S10P31A106-6' into 'main'"

--- backend/src/test/java/com/a106/rightlife/RightLifeApplicationTests.java
+++ backend/src/test/java/com/a106/rightlife/RightLifeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiaryApplicationTests {
+class RightLifeApplicationTests {
 
 	@Test
 	void contextLoads() {
