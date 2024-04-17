e5a8b96ed3f3a0be20c447d9a2e487507b7950d3
jsm0726 <jsm0726@naver.com>
2024-04-17 17:30:06 +0900

    S10P31A106-18 feat: GuideBox 컴포넌트 개발 및 스토리 작성

---
+++ frontend/src/components/common/GuideBox/GuideBox.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+interface GuideBoxProps {
+  guideText: string;
+  onClick?: () => void;
+}
+
+export const GuideBox = ({ guideText, ...props }: GuideBoxProps) => {
+  return (
+    <section
+      className={
+        'w-[34rem] h-[6rem] p-[1rem] flex gap-[1rem] items-center justify-center rounded-[1rem] bg-custom-light-gray'
+      }
+      {...props}
+    >
+      <Image
+        src={'/images/icon-clock.png'}
+        width={40}
+        height={40}
+        alt={'clock'}
+        className="w-[4rem] h-[4rem]"
+      ></Image>
+      <p className="custom-light-text">{guideText}</p>
+    </section>
+  );
+};
