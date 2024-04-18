5dfb2cb378a964bf2442cb00ef64e208172c47d0
김형진 <rlagudwls3469@gmail.com>
2024-04-18 17:42:28 +0900

    feat: Streak / Calender 컴포넌트 개발

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,24 @@
+import Calender from '@/components/Calender/Calender';
+import { StreaksResponse, setDayInfo } from '@/components/Calender/mockserver';
 import { NavBar } from '@/components/common/NavBar/NavBar';
 
 export default function Home() {
+  const { dayOfWeekFirst, memberHabitList, dayInfo } = StreaksResponse;
+  const theme = 'bareun-sanghwal';
+  const theme1 = 'rose';
   return (
     <>
-      <main className="custom-bold-text text-custom-light-green">
-        hello, world!
-      </main>
+      <>
+        <main className="custom-bold-text text-custom-light-green">
+          hello, world!
+        </main>
+        <Calender
+          dayOfWeekFirst={dayOfWeekFirst}
+          memberHabitList={memberHabitList}
+          dayInfo={dayInfo}
+          themeColor={theme1}
+        />
+      </>
       <NavBar mode="HOME" />
     </>
   );
