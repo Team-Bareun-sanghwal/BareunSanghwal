6398e6d6b79e68a2bc2b841abd429700b1903e80
곽희웅 <gmldnd567@naver.com>
2024-04-18 15:30:19 +0900

    S10P31A106-2 fix: member 코드 추가

--- backend/src/main/java/life/bareun/diary/habit/dto/HabitTrackerCreateDto.java
+++ backend/src/main/java/life/bareun/diary/habit/dto/HabitTrackerCreateDto.java
@@ -2,11 +2,14 @@ package life.bareun.diary.habit.dto;
 
 import java.time.LocalDate;
 import life.bareun.diary.habit.entity.MemberHabit;
+import life.bareun.diary.member.entity.Member;
 import lombok.Builder;
 
 @Builder
 public record HabitTrackerCreateDto(
 
+    Member member,
+    
     MemberHabit memberHabit,
 
     LocalDate targetDay,
