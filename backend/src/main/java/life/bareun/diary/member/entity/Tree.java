916eae2aa5cf0ec5b13a6027996163347557f850
신종욱 <rmm1@naver.com>
2024-04-18 09:33:20 +0900

    feat: Member, Tree 엔티티 추가

---
+++ backend/src/main/java/life/bareun/diary/member/entity/Tree.java
@@ -0,0 +1,34 @@
+package life.bareun.diary.member.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tree")
+public class Tree {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "level")
+    @Min(0)
+    private Integer level;
+
+    @Column(name = "capacity")
+    @Min(0)
+    private Integer capacity;
+}
