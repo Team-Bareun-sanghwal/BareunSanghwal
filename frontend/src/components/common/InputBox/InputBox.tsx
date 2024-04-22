ebb873fd79ae1420aee16fae34dcdbb4a369faba
jsm0726 <jsm0726@naver.com>
2024-04-22 10:27:47 +0900

    S10P31A106-18 fix: InputBox의 input 하단 라인 색상, 가이드 텍스트 색상, 가이드 텍스트를 useRef 대신 useState로 관리

--- frontend/src/components/common/InputBox/InputBox.tsx
+++ frontend/src/components/common/InputBox/InputBox.tsx
@@ -8,15 +8,23 @@ export const InputBox = ({ mode }: IInputBoxProps) => {
   const label = mode === 'NICKNAME' ? '닉네임' : '해빗 별칭';
   const placeholder =
     mode === 'NICKNAME'
-      ? '2~10글자로 입력해주세요.'
+      ? '2~12자로 입력해주세요.'
       : '15자 이하로 입력해주세요.';
   const regExp =
     mode === 'NICKNAME'
-      ? /[A-Za-z0-9ㄱ-ㅎ가-힣!?@#$%^&*]{2,10}/g
-      : /[A-Za-z0-9ㄱ-ㅎ가-힣!?@#$%^&*]{1,15}/g;
-  let lineColor = useRef<string>('border-b-custom-green');
-  let guideText = useRef<string | null>(null);
-  let guideTextColor = useRef<string>('text-custom-black');
+      ? /^[A-Za-z0-9ㄱ-ㅎ가-힣!?@#$%^&*\s]{2,12}$/
+      : /^[A-Za-z0-9ㄱ-ㅎ가-힣!?@#$%^&*\s]{1,15}$/;
+
+  const [lineColor, setLineColor] = useState<string>('border-b-custom-error');
+
+  const [guideText, setGuideText] = useState<string>(() =>
+    mode === 'NICKNAME'
+      ? '닉네임이 없습니다. 입력해주세요.'
+      : '해빗 별칭이 없습니다. 입력해주세요.',
+  );
+
+  const [guideTextColor, setGuideTextColor] =
+    useState<string>('text-custom-error');
 
   const [userInputData, setUserInputData] = useState<string | null>(null);
 
@@ -24,45 +32,48 @@ export const InputBox = ({ mode }: IInputBoxProps) => {
     <section className="w-[34rem] flex flex-col items-start gap-[0.5rem]">
       <label className="custom-semibold-text text-custom-matcha">{label}</label>
       <input
-        className={`${lineColor.current} w-[34rem] bg-transparent custom-medium-text outline-none py-[0.5rem] border-b-[0.2rem]`}
+        className={`${lineColor} w-[34rem] bg-transparent custom-medium-text outline-none py-[0.5rem] border-b-[0.2rem]`}
         placeholder={placeholder}
         onChange={(event) => {
-          // if (event.target.value) {
-          //   if (regExp.test(event.target.value)) {
-          //     setUserInputData(event.target.value);
-          //     lineColor.current = 'border-b-custom-success';
-          //     guideTextColor.current = 'text-custom-success';
-          //     if (mode === 'NICKNAME') {
-          //       guideText.current = '좋은 닉네임이네요!';
-          //     } else if (mode === 'HABITNICKNAME') {
-          //       guideText.current = '좋은 해빗 별칭입니다!';
-          //     }
-          //   } else {
-          //     setUserInputData(null);
-          //     lineColor.current = 'border-b-custom-error';
-          //     guideTextColor.current = 'text-custom-error';
-          //     if (mode === 'NICKNAME') {
-          //       guideText.current =
-          //         '2~10자의 한글, 영문, 숫자, 특수 기호 조합의 닉네임을 정해주세요.';
-          //     } else if (mode === 'HABITNICKNAME') {
-          //       guideText.current =
-          //         '15자 이하의 한글, 영문, 숫자, 특수 기호 조합의 닉네임을 정해주세요.';
-          //     }
-          //   }
-          // } else {
-          //   lineColor.current = 'border-b-custom-error';
-          //   guideTextColor.current = 'text-custom-error';
-          //   if (mode === 'NICKNAME') {
-          //     guideText.current = '닉네임이 없습니다. 입력해주세요.';
-          //   } else if (mode === 'HABITNICKNAME') {
-          //     guideText.current = '해빗 별칭이 없습니다. 입력해주세요.';
-          //   }
-          // }
+          if (
+            event.target.value.length !== 0 &&
+            event.target.value.replaceAll(' ', '').length !== 0
+          ) {
+            if (regExp.test(event.target.value)) {
+              setUserInputData(event.target.value);
+              setLineColor('border-b-custom-success');
+              setGuideTextColor('text-custom-success');
+              if (mode === 'NICKNAME') {
+                setGuideText('좋은 닉네임이네요!');
+              } else if (mode === 'HABITNICKNAME') {
+                setGuideText('좋은 해빗 별칭입니다!');
+              }
+            } else {
+              setUserInputData(null);
+              setLineColor('border-b-custom-error');
+              setGuideTextColor('text-custom-error');
+              if (mode === 'NICKNAME') {
+                setGuideText(
+                  '2~12자의 한글, 영문, 숫자, 특수 기호 조합의 닉네임을 정해주세요.',
+                );
+              } else if (mode === 'HABITNICKNAME') {
+                setGuideText(
+                  '15자 이하의 한글, 영문, 숫자, 특수 기호 조합의 닉네임을 정해주세요.',
+                );
+              }
+            }
+          } else {
+            setLineColor('border-b-custom-error');
+            setGuideTextColor('text-custom-error');
+            if (mode === 'NICKNAME') {
+              setGuideText('닉네임이 없습니다. 입력해주세요.');
+            } else if (mode === 'HABITNICKNAME') {
+              setGuideText('해빗 별칭이 없습니다. 입력해주세요.');
+            }
+          }
         }}
       ></input>
-      <span className={`${guideTextColor.current} custom-light-text`}>
-        {guideText.current}
-      </span>
+      <span className={`${guideTextColor} custom-light-text`}>{guideText}</span>
     </section>
   );
 };
