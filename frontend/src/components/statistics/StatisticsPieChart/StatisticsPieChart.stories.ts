c6d770e9d9443343d1e45c2d7bdc99c6275fe09d
유현지 <ccamy0915@naver.com>
2024-04-22 12:53:23 +0900

    S10P31A106-18 feat: achievement data props 추가

--- frontend/src/components/statistics/StatisticsPieChart/StatisticsPieChart.stories.ts
+++ frontend/src/components/statistics/StatisticsPieChart/StatisticsPieChart.stories.ts
@@ -11,11 +11,41 @@ const meta = {
     },
   },
   tags: ['autodocs'],
+  argTypes: {
+    data: { description: 'achievement 데이터' },
+  },
 } satisfies Meta<typeof StatisticsPieChart>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Statistics_PieChart: Story = {
-  parameters: {},
+  args: {
+    data: [
+      {
+        x: '위스키 마시기',
+        y: 27,
+      },
+      {
+        x: '와인 마시기',
+        y: 21,
+      },
+      {
+        x: '악기 연주',
+        y: 18,
+      },
+      {
+        x: '요리하기',
+        y: 15,
+      },
+      {
+        x: '축구',
+        y: 11,
+      },
+      {
+        x: '기타',
+        y: 8,
+      },
+    ],
+  },
 };
