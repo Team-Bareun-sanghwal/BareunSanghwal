0859bea1ae3550d785de1715645f8049de4abe10
곽희웅 <gmldnd567@naver.com>
2024-04-22 15:06:26 +0900

    S10P31A106-2 feat: habit errorCode 추가

--- backend/src/main/java/life/bareun/diary/habit/exception/HabitErrorCode.java
+++ backend/src/main/java/life/bareun/diary/habit/exception/HabitErrorCode.java
@@ -12,7 +12,9 @@ public enum HabitErrorCode {
 
     NOT_FOUND_MEMBER_HABIT(HttpStatus.NOT_FOUND, "존재하지 않는 사용자 해빗입니다"),
 
-    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다");
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다"),
+
+    NOT_FOUND_HABIT_TRACKER(HttpStatus.NOT_FOUND, "존재하지 않는 해빗 트래커입니다.");
 
     private final HttpStatus status;
     private final String message;
