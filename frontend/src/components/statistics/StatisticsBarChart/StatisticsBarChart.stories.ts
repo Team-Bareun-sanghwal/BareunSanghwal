fec877b5220c9fe5568f29c9101ddb05a793bf87
유현지 <ccamy0915@naver.com>
2024-04-22 16:56:36 +0900

    S10P31A106-18 feat: data props 추가

--- frontend/src/components/statistics/StatisticsBarChart/StatisticsBarChart.stories.ts
+++ frontend/src/components/statistics/StatisticsBarChart/StatisticsBarChart.stories.ts
@@ -11,11 +11,52 @@ const meta = {
     },
   },
   tags: ['autodocs'],
+  argTypes: {
+    data: { description: 'achievement_per_day_week 데이터' },
+  },
 } satisfies Meta<typeof StatisticsBarChart>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Statistics_PieChart: Story = {
-  parameters: {},
+  args: {
+    data: [
+      {
+        day: '일',
+        value: 45,
+        colorIdx: 1,
+      },
+      {
+        day: '월',
+        value: 67,
+        colorIdx: 2,
+      },
+      {
+        day: '화',
+        value: 56,
+        colorIdx: 1,
+      },
+      {
+        day: '수',
+        value: 53,
+        colorIdx: 1,
+      },
+      {
+        day: '목',
+        value: 32,
+        colorIdx: 1,
+      },
+      {
+        day: '금',
+        value: 11,
+        colorIdx: 0,
+      },
+      {
+        day: '토',
+        value: 12,
+        colorIdx: 1,
+      },
+    ],
+  },
 };
