d4b5da5b814ae998c38ac420a1381f4ebe88e034
곽희웅 <gmldnd567@naver.com>
2024-04-23 17:39:20 +0900

    S10P31A106-5 feat: Error 로직 추가

---
+++ backend/src/main/java/life/bareun/diary/recap/exception/RecapErrorCode.java
@@ -0,0 +1,17 @@
+package life.bareun.diary.recap.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum RecapErrorCode {
+
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다"),
+
+    NOT_FOUND_HABIT(HttpStatus.NOT_FOUND, "존재하지 않는 해빗입니다");
+
+    private final HttpStatus status;
+    private final String message;
+}
