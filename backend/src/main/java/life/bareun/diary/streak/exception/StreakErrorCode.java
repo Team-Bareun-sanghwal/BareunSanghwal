a5fe7fc6d074d441cb0865440007c4b89e5391ae
전영빈 <binkorea2@gmail.com>
2024-04-23 17:40:36 +0900

    S10P31A106-223 fix: 스트릭 예외 에러 코드 분리
    스트릭 에러 코드가 많아질 것으로 예상하여 스트릭 서비스별 에러 코드를 분리

--- backend/src/main/java/life/bareun/diary/streak/exception/StreakErrorCode.java
+++ backend/src/main/java/life/bareun/diary/streak/exception/StreakErrorCode.java
@@ -1,18 +1,10 @@
 package life.bareun.diary.streak.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-@AllArgsConstructor
-public enum StreakErrorCode {
+public interface StreakErrorCode {
 
-    NOT_FOUND_MEMBER_TOTAL_STREAK(HttpStatus.NOT_FOUND, "해당 멤버의 전체 스트릭이 존재하지 않습니다."),
-    ALREADY_EXISTED_MEMBER_TOTAL_STREAK(HttpStatus.FORBIDDEN, "해당 멤버의 전체 스트릭이 이미 존재합니다."),
-    NOT_FOUND_MEMBER_DAILY_STREAK(HttpStatus.NOT_FOUND, "해당 날짜의 멤버 일일 스트릭이 존재하지 않습니다."),
-    ALREADY_EXISTED_MEMBER_DAILY_STREAK(HttpStatus.FORBIDDEN, "해당 날짜의 멤버 일일 스트릭이 이미 존재합니다.");
+    HttpStatus getStatus();
 
-    private final HttpStatus status;
-    private final String message;
+    String getMessage();
 }
