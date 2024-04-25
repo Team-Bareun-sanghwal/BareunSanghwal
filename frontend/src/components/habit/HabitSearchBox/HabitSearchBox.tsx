0dde1b69a280493415f3ae28a281af68f8d91690
jsm0726 <jsm0726@naver.com>
2024-04-25 11:18:05 +0900

    S10P31A106-18 feat: HabitSearchBox 컴포넌트에 사용자 입력 유효성 검사 로직 추가

--- frontend/src/components/habit/HabitSearchBox/HabitSearchBox.tsx
+++ frontend/src/components/habit/HabitSearchBox/HabitSearchBox.tsx
@@ -16,6 +16,8 @@ const HabitNameButton = ({ name }: { name: string }) => {
 };
 
 export const HabitSearchBox = ({ searchedList }: IHabitSearchBoxProps) => {
+  const regExp = /^[ㄱ-ㅎ가-힣\s]+$/;
+
   return (
     <section className="w-full flex flex-col gap-[1.5rem]">
       <label className="custom-semibold-text text-custom-black">
@@ -25,6 +27,17 @@ export const HabitSearchBox = ({ searchedList }: IHabitSearchBoxProps) => {
       <input
         type="text"
         className="w-full px-[1.5rem] py-[0.7rem] rounded-[3rem] bg-transparent custom-medium-text outline-none border-[0.1rem] border-custom-medium-gray"
+        onChange={(event) => {
+          // 아래 경우에 검색 API 사용
+          if (
+            event.target.value.length !== 0 &&
+            event.target.value.replaceAll(' ', '').length !== 0
+          ) {
+            if (regExp.test(event.target.value)) {
+              console.log('검색 진행');
+            }
+          }
+        }}
       ></input>
 
       <div className="flex gap-[1rem] flex-wrap -mt-[0.5rem]">
