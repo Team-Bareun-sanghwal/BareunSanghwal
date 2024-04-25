f712ce819205cfe873fa4e8d566a27cd4077d0e5
jsm0726 <jsm0726@naver.com>
2024-04-25 16:42:11 +0900

    S10P31A106-18 feat: HabitDayChart 컴포넌트에서 해빗 별칭과 날짜 리스트를 분리하고, 해빗 별칭 클릭에 따라 날짜 리스트를 포커스하도록 구현

--- frontend/src/components/habit/HabitDayChart/HabitDayChart.tsx
+++ frontend/src/components/habit/HabitDayChart/HabitDayChart.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface IHabitDayData {
   name: string;
   habitId: number;
@@ -11,15 +13,19 @@ interface IHabitDayChartProps {
 const HabitDayBox = ({
   isChecked,
   koreanDayName,
+  isFocused,
 }: {
   isChecked: boolean;
   koreanDayName: string;
+  isFocused: boolean;
 }) => {
   return (
-    <div className="w-full h-[3rem] my-auto px-[0.3rem] py-[0.7rem]">
+    <div className="w-full h-[2rem] my-auto px-[0.3rem] py-[0.4rem]">
       {isChecked && (
-        <div className="w-full h-full bg-custom-light-green rounded-[1rem] text-[1rem] font-semibold flex items-center justify-center">
-          {koreanDayName}
+        <div
+          className={`${isFocused ? 'bg-custom-light-green' : 'bg-custom-light-gray'} w-full h-full rounded-[1rem] text-[1rem] font-semibold flex items-center justify-center`}
+        >
+          {/* {koreanDayName} */}
         </div>
       )}
     </div>
@@ -29,30 +35,62 @@ const HabitDayBox = ({
 const HabitDayRow = ({
   name,
   dayList,
+  isFocused,
 }: {
   name: string;
   dayList: number[];
+  isFocused: boolean;
 }) => {
   return (
     <div className="flex flex-col gap-[0.5rem]">
-      <label className="text-[1.2rem] font-bold">{name}</label>
+      {/* <label className="text-[1.2rem] font-bold">{name}</label> */}
 
       <div className="flex divide-dashed divide-x-[0.1rem] divide-custom-medium-gray">
-        <HabitDayBox isChecked={dayList.includes(1)} koreanDayName="월" />
-        <HabitDayBox isChecked={dayList.includes(2)} koreanDayName="화" />
-        <HabitDayBox isChecked={dayList.includes(3)} koreanDayName="수" />
-        <HabitDayBox isChecked={dayList.includes(4)} koreanDayName="목" />
-        <HabitDayBox isChecked={dayList.includes(5)} koreanDayName="금" />
-        <HabitDayBox isChecked={dayList.includes(6)} koreanDayName="토" />
-        <HabitDayBox isChecked={dayList.includes(7)} koreanDayName="일" />
+        <HabitDayBox
+          isChecked={dayList.includes(1)}
+          isFocused={isFocused}
+          koreanDayName="월"
+        />
+        <HabitDayBox
+          isChecked={dayList.includes(2)}
+          isFocused={isFocused}
+          koreanDayName="화"
+        />
+        <HabitDayBox
+          isChecked={dayList.includes(3)}
+          isFocused={isFocused}
+          koreanDayName="수"
+        />
+        <HabitDayBox
+          isChecked={dayList.includes(4)}
+          isFocused={isFocused}
+          koreanDayName="목"
+        />
+        <HabitDayBox
+          isChecked={dayList.includes(5)}
+          isFocused={isFocused}
+          koreanDayName="금"
+        />
+        <HabitDayBox
+          isChecked={dayList.includes(6)}
+          isFocused={isFocused}
+          koreanDayName="토"
+        />
+        <HabitDayBox
+          isChecked={dayList.includes(7)}
+          isFocused={isFocused}
+          koreanDayName="일"
+        />
       </div>
     </div>
   );
 };
 
 export const HabitDayChart = ({ habitList }: IHabitDayChartProps) => {
+  const [selectedHabitId, setSelectedHabitId] = useState<number | null>(null);
+
   return (
-    <section className="w-full flex flex-col gap-[1rem]">
+    <section className="w-full flex flex-col gap-[0.5rem]">
       <label className="custom-semibold-text text-custom-black">
         현재 진행 중인 해빗의 요일
       </label>
@@ -63,9 +101,33 @@ export const HabitDayChart = ({ habitList }: IHabitDayChartProps) => {
             key={`habitDay-${index}`}
             name={habit.name}
             dayList={habit.habitDayList}
+            isFocused={
+              selectedHabitId ? habit.habitId === selectedHabitId : false
+            }
           />
         );
       })}
+
+      <div className="my-[2rem] grid grid-cols-2 mx-auto">
+        {habitList?.map((habit, index) => {
+          return (
+            <div
+              key={`habitName-${index}`}
+              className="flex gap-[0.5rem] items-center cursor-pointer justify-center px-[1rem] py-[0.5rem]"
+              onClick={() => setSelectedHabitId(habit.habitId)}
+            >
+              <div
+                className={`${habit.habitId === selectedHabitId ? 'bg-custom-light-green' : 'bg-custom-medium-gray'} w-[0.8rem] h-[0.8rem] rounded-full`}
+              ></div>
+              <span
+                className={`${habit.habitId === selectedHabitId ? 'text-[1.1rem] font-semibold' : 'text-[1.1rem] font-light'}`}
+              >
+                {habit.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
