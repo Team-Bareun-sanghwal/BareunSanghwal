7eae5f4cdcf95cb762e6656bb3aea492c55c486f
유현지 <ccamy0915@naver.com>
2024-04-25 18:11:08 +0900

    S10P31A106-18 feat: RecapBarChart 컴포넌트 제작(ing)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,49 @@
 import { NavBar } from '@/components/common/NavBar/NavBar';
+import { RecapBarChart } from '@/components/recap/RecapBarChart/RecapBarChart';
+
+const data = [
+  {
+    habit: '12시 취침',
+    missCount: 2,
+    actionCount: 15,
+    ratio: 80,
+  },
+  {
+    habit: '영양제 먹기',
+    missCount: 3,
+    actionCount: 27,
+    ratio: 90,
+  },
+  {
+    habit: '점심 샐러드 먹기',
+    missCount: 8,
+    actionCount: 27,
+    ratio: 78,
+  },
+  {
+    habit: '강의장 계단으로 가기',
+    missCount: 3,
+    actionCount: 37,
+    ratio: 93,
+  },
+  {
+    habit: '웨이트 1시간',
+    missCount: 5,
+    actionCount: 37,
+    ratio: 89,
+  },
+];
 
 export default function Home() {
   return (
-    <>
-      <main className="custom-bold-text text-custom-light-green">
+    <div>
+      <div className="h-72 bg-custom-black"></div>
+      <RecapBarChart rateByHabitList={data} />
+      {/* <main className="custom-bold-text text-custom-light-green">
         hello, world!
-      </main>
+      </main> */}
 
-      <NavBar mode="HOME" />
-    </>
+      {/* <NavBar mode="HOME" /> */}
+    </div>
   );
 }
--- frontend/src/components/recap/RecapBarChart/RecapBarChart.tsx
+++ frontend/src/components/recap/RecapBarChart/RecapBarChart.tsx
@@ -9,8 +9,10 @@ import {
   Legend3D,
   DataLabel3D,
   Tooltip3D,
-  ColumnSeries3D,
   Highlight3D,
+  Chart3DAxisModel,
+  StackingColumnSeries3D,
+  LegendPosition,
 } from '@syncfusion/ej2-react-charts';
 import { registerLicense } from '@syncfusion/ej2-base';
 
@@ -20,33 +22,105 @@ registerLicense(
     : '',
 );
 
-const data = [
-  { country: 'USA', gold: 50, silver: 70, bronze: 45 },
-  { country: 'China', gold: 40, silver: 60, bronze: 55 },
-  { country: 'Japan', gold: 70, silver: 60, bronze: 50 },
-  { country: 'Australia', gold: 60, silver: 56, bronze: 40 },
-  { country: 'France', gold: 50, silver: 45, bronze: 35 },
-  { country: 'Germany', gold: 40, silver: 30, bronze: 22 },
-  { country: 'Italy', gold: 40, silver: 35, bronze: 37 },
-  { country: 'Sweden', gold: 30, silver: 25, bronze: 27 },
-];
+interface IDataType {
+  habit: string;
+  missCount: number;
+  actionCount: number;
+  ratio: number;
+}
 
-export const RecapBarChart = () => {
+// interface legendSettingsType {
+//   visible: boolean;
+//   shapeHeight: number;
+//   shapeWidth: number;
+//   position: LegendPosition;
+//   width: string;
+//   textStyle: {
+//     fontFamily: string;
+//     fontWeight: string;
+//     size: string;
+//   };
+// }
+
+// const legendSettings = {
+//   visible: false,
+// };
+
+const xAxisConfig: Chart3DAxisModel = {
+  valueType: 'Category',
+  visible: true,
+  majorGridLines: { width: 0 },
+  majorTickLines: { width: 0 },
+};
+
+const yAxisConfig: Chart3DAxisModel = {
+  visible: false,
+  majorGridLines: { width: 0 },
+  majorTickLines: { width: 0 },
+};
+
+const pointRender = (args) => {
+  let seriesColor = [
+    '#00bdae',
+    '#404041',
+    '#357cd2',
+    '#e56590',
+    '#f8b883',
+    '#70ad47',
+    '#dd8abd',
+    '#7f84e8',
+    '#7bb4eb',
+    '#ea7a57',
+  ];
+  args.fill = seriesColor[2 * args.point.index + args.point.series.index];
+
+  // console.log(args.point.series.properties.dataLabel.properties.format);
+  // args.point.series.properties.dataLabel.properties.format = '{value}잉잉';
+};
+
+const textRender = (args) => {
+  console.log(args.point.index);
+  console.log(args.point.series.properties.dataSource[args.point.index]);
+  const indexedData = args.point.series.properties.dataSource[args.point.index];
+  args.text = `${indexedData.habit} <br> ${indexedData.ratio}%`;
+};
+
+const labelSettings = {
+  visible: true,
+  position: 'Bottom',
+  font: {
+    fontFamily: 'pretendard, sans-serif',
+    fontWeight: '200',
+    size: '12',
+    color: 'white',
+  },
+  margin: {
+    bottom: 20,
+  },
+};
+
+export const RecapBarChart = ({
+  rateByHabitList,
+}: {
+  rateByHabitList: IDataType[];
+}) => {
   return (
     <Chart3DComponent
-      id="charts"
-      primaryXAxis={{
-        valueType: 'Category',
-      }}
+      id="RecapBarChart"
+      primaryXAxis={xAxisConfig}
+      primaryYAxis={yAxisConfig}
+      pointRender={pointRender}
+      textRender={textRender}
       wallColor="transparent"
-      enableRotation={true}
-      rotation={7}
-      tilt={10}
+      enableRotation={false}
+      rotation={20}
+      perspectiveAngle={300}
       depth={100}
+      className="w-full bg-custom-black"
     >
       <Inject
         services={[
-          ColumnSeries3D,
+          StackingColumnSeries3D,
           Category3D,
           Legend3D,
           Tooltip3D,
@@ -56,11 +130,21 @@ export const RecapBarChart = () => {
       />
       <Chart3DSeriesCollectionDirective>
         <Chart3DSeriesDirective
-          dataSource={data}
-          xName="country"
-          yName="gold"
-          type="Column"
+          dataSource={rateByHabitList}
+          xName="habit"
+          yName="actionCount"
+          type="StackingColumn"
+          columnSpacing={-0.4}
           fill="red"
+        ></Chart3DSeriesDirective>
+        <Chart3DSeriesDirective
+          dataSource={rateByHabitList}
+          xName="habit"
+          yName="missCount"
+          type="StackingColumn"
+          columnSpacing={-0.4}
+          fill="green"
+          dataLabel={labelSettings}
         ></Chart3DSeriesDirective>
       </Chart3DSeriesCollectionDirective>
     </Chart3DComponent>
