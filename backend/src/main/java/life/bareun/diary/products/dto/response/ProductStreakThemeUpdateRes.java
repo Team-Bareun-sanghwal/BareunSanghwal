468522c2713fa29f86f72e33998fbbc939924f8a
신종욱 <rmm1@naver.com>
2024-04-25 17:57:07 +0900

    feat: 스트릭 변경권 구매 응답 객체 추가

---
+++ backend/src/main/java/life/bareun/diary/products/dto/response/ProductStreakThemeUpdateRes.java
@@ -0,0 +1,10 @@
+package life.bareun.diary.products.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ProductStreakThemeUpdateRes {
+    private String streakColorName;
+}
