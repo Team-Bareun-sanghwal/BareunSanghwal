c8fe72379d3cb9dc4370e010a9739035ec2ea498
신종욱 <rmm1@naver.com>
2024-04-25 14:27:57 +0900

    feat: 상품 목록 API 추가

---
+++ backend/src/main/java/life/bareun/diary/member/repository/MemberRecoveryRepository.java
@@ -0,0 +1,8 @@
+package life.bareun.diary.member.repository;
+
+import life.bareun.diary.member.entity.MemberRecovery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRecoveryRepository extends JpaRepository<MemberRecovery, Long> {
+
+}
--- backend/src/main/java/life/bareun/diary/products/service/ProductServiceImpl.java
+++ backend/src/main/java/life/bareun/diary/products/service/ProductServiceImpl.java
@@ -1,24 +1,44 @@
 package life.bareun.diary.products.service;
 
 import java.util.List;
+import life.bareun.diary.global.security.util.AuthUtil;
+import life.bareun.diary.member.exception.MemberErrorCode;
+import life.bareun.diary.member.exception.MemberException;
+import life.bareun.diary.member.repository.MemberRecoveryRepository;
 import life.bareun.diary.products.dto.ProductDto;
 import life.bareun.diary.products.dto.response.ProductListResDto;
-import life.bareun.diary.products.entity.Product;
 import life.bareun.diary.products.mapper.ProductMapper;
 import life.bareun.diary.products.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final MemberRecoveryRepository memberRecoveryRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public ProductListResDto productList() {
+        Long id = AuthUtil.getMemberIdFromAuthentication();
+        int freeRecoveryCount = memberRecoveryRepository.findById(id)
+            .orElseThrow(
+                () -> new MemberException(MemberErrorCode.NO_SUCH_USER)
+            )
+            .getFreeRecovery();
+
         List<ProductDto> products = productRepository.findAll().stream()
             .map(ProductMapper.INSTANCE::toProductDto)
+            .peek(
+                productDto -> {
+                    if (productDto.getKey().equals("streak_recovery") && freeRecoveryCount > 0) {
+                        productDto.setPrice(0);
+                    }
+                }
+            )
             .toList();
 
         return new ProductListResDto(products);
