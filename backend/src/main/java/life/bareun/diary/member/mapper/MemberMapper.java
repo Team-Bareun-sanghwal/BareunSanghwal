ed270e2dee3a2601dc85519180d3e915f69f6c2a
신종욱 <rmm1@naver.com>
2024-04-26 13:51:21 +0900

    feat: 사용자 객체 매퍼 추가

---
+++ backend/src/main/java/life/bareun/diary/member/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package life.bareun.diary.member.mapper;
+
+import life.bareun.diary.member.dto.response.MemberInfoRes;
+import life.bareun.diary.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MemberMapper {
+    MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
+
+    @Mapping(source = "nickname", target = "nickname")
+    @Mapping(source = "gender", target = "gender")
+    @Mapping(source = "job", target = "job")
+    @Mapping(source = "birth", target = "birthDate")
+    MemberInfoRes toMemberInfoRes(Member member);
+}
