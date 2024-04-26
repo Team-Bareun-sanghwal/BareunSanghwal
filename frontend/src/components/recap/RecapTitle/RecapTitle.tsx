718c99b5209ddc4329dc8ac001f740f95c9379f4
유현지 <ccamy0915@naver.com>
2024-04-26 16:37:35 +0900

    S10P31A106-18 design: 글자 색상 흰색으로 변경

--- frontend/src/components/recap/RecapTitle/RecapTitle.tsx
+++ frontend/src/components/recap/RecapTitle/RecapTitle.tsx
@@ -6,7 +6,7 @@ export const RecapTitle = ({ title }: IPropType) => {
   return (
     <div className="w-full h-14 flex items-center">
       <div className="w-2 h-full mr-5 bg-custom-yellow-green" />
-      <p className="custom-bold-text h-fit">{title}</p>
+      <p className="text-white custom-bold-text h-fit">{title}</p>
     </div>
   );
 };
