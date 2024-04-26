132ff5285871bf46f9bb883c31d091fdedef8fb4
신종욱 <rmm1@naver.com>
2024-04-26 10:37:58 +0900

    feat: 나무 색상 엔티티 추가

---
+++ backend/src/main/java/life/bareun/diary/product/repository/TreeColorRepository.java
@@ -0,0 +1,8 @@
+package life.bareun.diary.product.repository;
+
+import life.bareun.diary.product.entity.TreeColor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreeColorRepository extends JpaRepository<TreeColor, Integer> {
+
+}
