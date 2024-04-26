59376336315afd17b3cd76fb4167a6e4cb597867
전영빈 <binkorea2@gmail.com>
2024-04-26 16:46:20 +0900

    S10P31A106-223 fix: 컨벤션에 맞지 않는 코드들 수정 및 todo 추가

--- backend/src/main/java/life/bareun/diary/streak/dto/response/HabitStreakResDto.java
+++ backend/src/main/java/life/bareun/diary/streak/dto/response/HabitStreakResDto.java
@@ -1,12 +1,10 @@
 package life.bareun.diary.streak.dto.response;
 
 import java.util.List;
-import life.bareun.diary.streak.dto.MemberHabitDto;
 import life.bareun.diary.streak.dto.StreakInfoByDayDto;
 import lombok.Builder;
 
 @Builder
-public record HabitStreakResDto(double achieveProportion, int dayOfWeekFirst, List<MemberHabitDto> memberHabitList,
-                                List<StreakInfoByDayDto> dayInfo) {
+public record HabitStreakResDto(double achieveProportion, int dayOfWeekFirst, List<StreakInfoByDayDto> dayInfo) {
 
 }
