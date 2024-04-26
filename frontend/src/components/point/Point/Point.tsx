d45f885ce815712fd495fab42224db07293ccf2a
김형진 <rlagudwls3469@gmail.com>
2024-04-26 10:27:16 +0900

    style: 아이템/포인트 배경색상 및 스토리북 수정

--- frontend/src/components/point/Point/Point.tsx
+++ frontend/src/components/point/Point/Point.tsx
@@ -1,14 +1,14 @@
 const Point = ({ point }: { point: number }) => {
   return (
-    <div className="flex border-solid border-2 border-custom-matcha p-1 text-l text-center content-center max-w-24 min-w-16 rounded-full">
+    <div className="flex border-solid border-2 border-custom-matcha bg-white p-1 text-l text-center content-center max-w-24 min-w-16 rounded-full">
       {point === -1 ? (
-        <div>free</div>
+        <div className="w-full font-extrabold text-center">FREE</div>
       ) : (
         <>
           <div className="bg-custom-matcha w-6 h-6 rounded-full text-white text-center content-center mr-2">
             P
           </div>
-          <p className="flex text-right ml-auto mr-1 text-custom-matcha">
+          <p className="flex text-right ml-auto mr-2 text-custom-matcha">
             {point}
           </p>
         </>
