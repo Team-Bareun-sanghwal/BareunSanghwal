808287707f7017ca4c619382f872fe88a18a7d0d
김형진 <rlagudwls3469@gmail.com>
2024-04-29 11:07:22 +0900

    design: outdoor asset적용

--- frontend/src/components/tree/Tree.tsx
+++ frontend/src/components/tree/Tree.tsx
@@ -7,6 +7,7 @@ import * as THREE from 'three';
 
 function Model({ color, rotation }: { color: string; rotation: number }) {
   const { scene } = useGLTF('/assets/orgTree.glb');
+  scene.position.set(-4.2, 0.2, -3);
   scene.traverse((child: THREE.Object3D) => {
     if ((child as THREE.Mesh).isMesh) {
       const name = child.name;
@@ -44,27 +45,13 @@ function Model({ color, rotation }: { color: string; rotation: number }) {
 
   return <primitive object={scene} rotation={[0, rotation, 0]} />;
 }
-
+function OutDoor() {
+  const { scene } = useGLTF('/assets/outdoor.glb');
+  return <primitive object={scene} />;
+}
 export default function Tree({ color }: { color: string }) {
   const [rotation, setRotation] = useState(0);
   const rotateRef = useRef({ rotate: false, direction: 0 });
-
-  useEffect(() => {
-    const animate = () => {
-      if (rotateRef.current.rotate) {
-        console.log('animate  ');
-        setRotation(
-          (rotation) => rotation + 0.01 * rotateRef.current.direction,
-        );
-        requestAnimationFrame(animate);
-      }
-    };
-
-    requestAnimationFrame(animate);
-    return () => {
-      rotateRef.current.rotate = false;
-    };
-  }, []);
 
   const startRotation = (direction: number) => {
     console.log('startRotation');
@@ -72,38 +59,31 @@ export default function Tree({ color }: { color: string }) {
     requestAnimationFrame(() => {});
   };
 
-  const stopRotation = () => {
-    console.log('stopRotation');
-    rotateRef.current.rotate = false;
-  };
-
   return (
-    <div style={{ height: '500px', width: '100%' }}>
-      <button
-        onMouseDown={() => startRotation(-1)}
-        onMouseUp={stopRotation}
-        className="text-3xl border-2 border-black w-10 h-10 rounded-full text-center content-center top-10 ml-8"
-      >
-        A
-      </button>
-      <button
-        onMouseDown={() => startRotation(1)}
-        onMouseUp={stopRotation}
-        className="text-3xl border-2 border-black w-10 h-10 rounded-full text-center content-center top-10 ml-28"
-      >
-        B
-      </button>
-      <Canvas camera={{ position: [4, 2, 14], fov: 75, near: 1, far: 1000 }}>
-        <ambientLight intensity={0.3} />
-        <spotLight position={[10, 10, 10]} angle={0.4} penumbra={1} />
-        <directionalLight position={[-10, 10, 10]} intensity={2} />
-        <pointLight position={[-10, -10, -10]} />
-        <Suspense fallback={null}>
-          <Model color={color} rotation={rotation} />
-        </Suspense>
-        <OrbitControls />
-      </Canvas>
-      <Loader />
-    </div>
+    <>
+      <div style={{ height: '500px', width: '100%' }}>
+        <button
+          onClick={() => console.log('hi')}
+          className="text-3xl border-2 border-black w-10 h-10 rounded-full text-center content-center top-10 ml-8"
+        >
+          A
+        </button>
+        <button className="text-3xl border-2 border-black w-10 h-10 rounded-full text-center content-center top-10 ml-28">
+          B
+        </button>
+        <Canvas camera={{ position: [4, 4, 14], fov: 90, near: 1, far: 1000 }}>
+          <ambientLight intensity={0.6} />
+          <spotLight position={[10, 10, 10]} angle={0.8} penumbra={1} />
+          <directionalLight position={[-10, 10, 10]} intensity={2} />
+          <pointLight position={[-10, -10, -10]} />
+          <Suspense fallback={null}>
+            <Model color={color} rotation={rotation} />
+            <OutDoor />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+        <Loader />
+      </div>
+    </>
   );
 }
