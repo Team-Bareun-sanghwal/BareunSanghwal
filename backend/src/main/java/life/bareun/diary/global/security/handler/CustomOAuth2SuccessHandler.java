ecb4a0c90901040e99a43686049a632361bca894
신종욱 <rmm1@naver.com>
2024-04-29 13:56:53 +0900

    ref: 로그인 응답 수정

--- backend/src/main/java/life/bareun/diary/global/security/handler/CustomOAuth2SuccessHandler.java
+++ backend/src/main/java/life/bareun/diary/global/security/handler/CustomOAuth2SuccessHandler.java
@@ -4,17 +4,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import life.bareun.diary.global.common.response.BaseResponse;
+import life.bareun.diary.global.security.dto.response.AuthLoginRes;
 import life.bareun.diary.global.security.principal.OAuth2MemberPrincipal;
 import life.bareun.diary.global.security.token.AuthTokenProvider;
 import life.bareun.diary.global.security.util.ResponseUtil;
-import life.bareun.diary.member.dto.response.MemberLoginRes;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 // SimpleUrlAuthenticationSuccessHandler는 성공 후 특정 URL로 redirect하기 위해 쓰인다.
 @RequiredArgsConstructor
+@Slf4j
 public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final AuthTokenProvider authTokenProvider;
@@ -26,19 +28,27 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
         Authentication authentication
     ) throws IOException {
         OAuth2MemberPrincipal oAuth2MemberPrincipal = (OAuth2MemberPrincipal) authentication.getPrincipal();
-        String accessToken = authTokenProvider.createToken(oAuth2MemberPrincipal);
-        System.out.println("accessToken: " + accessToken);
+        String id = oAuth2MemberPrincipal.getName();
+        String role = oAuth2MemberPrincipal.getAuthority();
+        String accessToken = authTokenProvider.createAccessToken(id, role);
+        String refreshToken = authTokenProvider.createRefreshToken(id);
+        // System.out.println("accessToken: " + accessToken);
+        log.debug("accessToken: {}", accessToken);
+        log.debug("refreshToken: {}", refreshToken);
 
         // 응답
         int statusCode = oAuth2MemberPrincipal.isNewMember() ? HttpStatus.CREATED.value()
             : HttpStatus.OK.value();
-
+        AuthLoginRes authLoginRes = AuthLoginRes.builder()
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build();
         ResponseUtil.respondSuccess(
             response,
             BaseResponse.success(
                 statusCode,
                 "OAuth2 인증되었습니다.",
-                new MemberLoginRes(accessToken)
+                authLoginRes
             )
         );
     }
