1b5b62124a9fec7fe1338adf1e3acee28e9a482c
유현지 <ccamy0915@naver.com>
2024-04-29 18:18:41 +0900

    S10P31A106-18 feat: InfoModifyList 컴포넌트 제작 완료

---
+++ frontend/src/components/mypage/InfoModifyList/InfoModifyList.tsx
@@ -0,0 +1,43 @@
+import { InfoModify } from '../InfoModify/InfoModify';
+
+interface IPropType {
+  nickname: string;
+  birthDate: string;
+  gender: string;
+  job: string;
+}
+
+export const InfoModifyList = ({ userData }: { userData: IPropType }) => {
+  const mypageArr = [
+    {
+      title: '닉네임',
+      prevData: userData.nickname,
+    },
+    {
+      title: '성별',
+      prevData: userData.gender,
+    },
+    {
+      title: '직업',
+      prevData: userData.job,
+    },
+    {
+      title: '생일',
+      prevData: userData.birthDate,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-[1rem]">
+      {mypageArr.map((data) => {
+        return (
+          <InfoModify
+            key={data.title}
+            title={data.title}
+            prevData={data.prevData}
+          />
+        );
+      })}
+    </div>
+  );
+};
