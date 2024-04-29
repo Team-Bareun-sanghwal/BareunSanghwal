a4e8ed156bc8da9989b1e7bbe8a3141adb0ffc0e
신종욱 <rmm1@naver.com>
2024-04-29 16:36:13 +0900

    feat: RedisConfig 추가

--- backend/src/main/java/life/bareun/diary/global/security/service/AuthTokenServiceImpl.java
+++ backend/src/main/java/life/bareun/diary/global/security/service/AuthTokenServiceImpl.java
@@ -2,20 +2,29 @@ package life.bareun.diary.global.security.service;
 
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class AuthTokenServiceImpl implements AuthTokenService {
 
-    private final RedisTemplate<Long, String> redisTemplate;
+    private final RedisTemplate<Long, String> authRedisTemplate;
+
+    @Autowired
+    public AuthTokenServiceImpl(
+        @Qualifier(value = "authRedisTemplate")
+        RedisTemplate<Long, String> authRedisTemplate
+    ) {
+        this.authRedisTemplate = authRedisTemplate;
+    }
 
     @Override
     @Transactional
-    public void add(Long id, String value, Duration expiry) {
-        redisTemplate.opsForValue().set(id, value, expiry);
+    public void revoke(Long id, String value, Duration expiry) {
+        authRedisTemplate.opsForValue().set(id, value, expiry);
     }
 
     @Override
@@ -25,7 +34,7 @@ public class AuthTokenServiceImpl implements AuthTokenService {
     }
 
     @Transactional(readOnly = true)
-    private String findById(Long id) {
-        return redisTemplate.opsForValue().get(id);
+    protected String findById(Long id) {
+        return authRedisTemplate.opsForValue().get(id);
     }
 }
