a60ee2ab013792c709e046b3e41e7128bc67ca30
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-04-30 23:53:58 +0900

    feat: ColoredRecapTitle 컴포넌트 제작 완료

--- frontend/src/components/recap/ColoredRecapTitle/ColoredRecapTitle.tsx
+++ frontend/src/components/recap/ColoredRecapTitle/ColoredRecapTitle.tsx
@@ -1,13 +1,37 @@
 interface IPropType {
   title: string;
-  colorIdx: string;
+  colorIdx: 0 | 1 | 2 | 3 | 4;
 }
 
-const colorArr = ['#F8A4C8', '#DEA3C9', '#C3A2CA', '#7D9DCC', '#5B9ED0'];
-
 export const ColoredRecapTitle = ({ title, colorIdx }: IPropType) => {
-  // const textColor =
-  // console.log(textColor);
+  let textColor = '';
+  let textAlign = '';
+  switch (colorIdx) {
+    case 0:
+      textColor = 'text-[#F8A4C8]';
+      textAlign = 'ml-auto';
+      break;
+    case 1:
+      textColor = 'text-[#DEA3C9]';
+      textAlign = 'mr-auto';
+      break;
+    case 2:
+      textColor = 'text-[#C3A2CA]';
+      textAlign = 'mx-auto';
+      break;
+    case 3:
+      textColor = 'text-[#7D9DCC]';
+      textAlign = 'mr-auto';
+      break;
+    case 4:
+      textColor = 'text-[#5B9ED0]';
+      textAlign = 'ml-auto';
+      break;
+  }
 
-  return <p className={`${textColor}`}>{title}</p>;
+  return (
+    <p className={`${textColor} ${textAlign} custom-semibold-text text-center`}>
+      {title}
+    </p>
+  );
 };
