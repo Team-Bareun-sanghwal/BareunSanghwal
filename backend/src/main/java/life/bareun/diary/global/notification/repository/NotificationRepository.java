5def80106d8359fc466a51504f9127ba3a663733
곽희웅 <gmldnd567@naver.com>
2024-05-02 11:23:21 +0900

    S10P31A106-10 feat: 알림 목록 조회 API 구현

---
+++ backend/src/main/java/life/bareun/diary/global/notification/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package life.bareun.diary.global.notification.repository;
+
+import java.util.List;
+import life.bareun.diary.global.notification.entity.Notification;
+import life.bareun.diary.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationRepositoryCustom {
+    List<Notification> findAllByMember_OrderByCreatedDatetimeDescIdDesc(Member member);
+}
--- backend/src/main/java/life/bareun/diary/global/notification/service/NotificationService.java
+++ backend/src/main/java/life/bareun/diary/global/notification/service/NotificationService.java
@@ -1,9 +1,12 @@
 package life.bareun.diary.global.notification.service;
 
 import life.bareun.diary.global.notification.dto.request.NotificationReqDto;
+import life.bareun.diary.global.notification.dto.response.NotificationListResDto;
 
 public interface NotificationService {
     void createToken(NotificationReqDto notificationReqDto);
 
     void sendNotification();
+
+    NotificationListResDto findAllNotification();
 }
