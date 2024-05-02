043fb89323a14bbbba050bb3ace5205d1fec36a9
곽희웅 <gmldnd567@naver.com>
2024-05-02 10:09:35 +0900

    S10P31A106-2 fix: Timezone 수정

--- backend/src/main/java/life/bareun/diary/DiaryApplication.java
+++ backend/src/main/java/life/bareun/diary/DiaryApplication.java
@@ -1,5 +1,7 @@
 package life.bareun.diary;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,5 +13,9 @@ public class DiaryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiaryApplication.class, args);
     }
-
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+    }
 }
