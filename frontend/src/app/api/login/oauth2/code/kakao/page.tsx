35a712c85e94c96fcca4bad5677521005efaa273
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-02 22:58:40 +0900

    feat: GET => POST 변경

--- frontend/src/app/api/login/oauth2/code/kakao/page.tsx
+++ frontend/src/app/api/login/oauth2/code/kakao/page.tsx
@@ -2,11 +2,23 @@
 
 export default function Page() {
   let code = new URL(window.location.href).searchParams.get('code');
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao?code=${code}`;
+  const url = `https://bareun.life/api/oauth2/authorization/kakao?code=${code}`;
+  // const url = `${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao?code=${code}`;
 
-  const logIn = async () => {
-    const data = await (await fetch(url)).json();
-    console.log(data);
+  const logIn = () => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   logIn();
 
--- frontend/src/components/loading/LoginButton/LoginButton.tsx
+++ frontend/src/components/loading/LoginButton/LoginButton.tsx
@@ -11,22 +11,10 @@ export const LoginButton = ({ platform }: ILoginButtonProps) => {
   const router = useRouter();
 
   const signIn = () => {
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=82281de6e3c9869954034742a037b692&redirect_uri=https://bareun.life/api/login/oauth2/code/kakao&response_type=code`;
 
     router.push(KAKAO_AUTH_URL);
-    // const response = await axios.get(
-    //      `${API 서버 주소}/oauth/token?code=${code}`,
-    //      { withCredentials: true }
-    //    );
-
-    // const url =
-    //   platform === 'kakao'
-    //     ? process.env.NEXT_PUBLIC_OAUTH_KAKAO_URL!
-    //     : process.env.NEXT_PUBLIC_OAUTH_GOOGLE_URL!;
-    // router.push(url);
-    // console.log(1);
-    // console.log(2);
-    // console.log(3);
   };
 
   const bgColor = platform === 'kakao' ? 'bg-custom-kakao' : 'bg-custom-google';
