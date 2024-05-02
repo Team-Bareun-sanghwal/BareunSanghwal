993ddba8db977a0d9fe743632547a4c66fb88241
곽희웅 <gmldnd567@naver.com>
2024-05-02 09:45:35 +0900

    S10P31A106-2 fix: Tree id 자료형 수정

--- backend/src/main/java/life/bareun/diary/member/entity/Tree.java
+++ backend/src/main/java/life/bareun/diary/member/entity/Tree.java
@@ -20,7 +20,7 @@ public class Tree {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Integer id;
 
     @Column(name = "name")
     private String name;
