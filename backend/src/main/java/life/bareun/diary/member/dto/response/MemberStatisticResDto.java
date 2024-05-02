ed45f111190b8dad3921d2523eb64c7f12953ff7
신종욱 <rmm1@naver.com>
2024-05-02 15:17:54 +0900

    feat: 사용자 전체 해빗 통계 API 추가

--- backend/src/main/java/life/bareun/diary/member/dto/response/MemberStatisticResDto.java
+++ backend/src/main/java/life/bareun/diary/member/dto/response/MemberStatisticResDto.java
@@ -2,12 +2,16 @@ package life.bareun.diary.member.dto.response;
 
 import java.util.List;
 import life.bareun.diary.member.dto.MemberPracticeCountPerDayOfWeekDto;
+import life.bareun.diary.member.dto.MemberPracticeCountPerHourDto;
 import life.bareun.diary.member.dto.MemberPracticedHabitDto;
+import lombok.Builder;
 
+@Builder
 public record MemberStatisticResDto(
     List<MemberPracticedHabitDto> practicedHabitsTop,
     String maxPracticedHabit,
     List<MemberPracticeCountPerDayOfWeekDto> practiceCountsPerDayOfWeek,
+    List<MemberPracticeCountPerHourDto> practiceCountsPerHour,
     int totalDays,
     int streakDays,
     int starredDays,
