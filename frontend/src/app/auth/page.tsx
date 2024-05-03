f62e530dfe08817591415d0355970837c858b671
유현지 <ccamy0915@naver.com>
2024-05-03 15:39:58 +0900

    S10P31A106-9 feat: 토큰 출력

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,18 +1,21 @@
+import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
 import { cookies } from 'next/headers';
 
 export default function Page() {
   const cookieStore = cookies();
   const accessToken = cookieStore.get('Authorization');
   const refreshToken = cookieStore.get('RefreshToken');
+  console.log(accessToken);
+  console.log(refreshToken);
 
   return (
     <>
       <div>우우</div>
-      {accessToken ? <div>{accessToken.toString()}</div> : '어세스 토큰이 업서'}
+      {accessToken ? <div>{accessToken}</div> : <div>어세스 토큰이 업서</div>}
       {refreshToken ? (
-        <div>{refreshToken.toString()}</div>
+        <div>{refreshToken}</div>
       ) : (
-        '리프레시 토큰이 업서'
+        <div>리프레시 토큰이 업서</div>
       )}
     </>
   );
