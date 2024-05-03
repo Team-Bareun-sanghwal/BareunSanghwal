0e35a8a700df3f0a83a5597f1ccfce669fa30b80
신종욱 <rmm1@naver.com>
2024-05-03 16:43:09 +0900

    feat: 사용자 해빗 목록을 생성 날짜 기준으로 내림차순 정렬해서 읽어오는 메소드 추가

--- backend/src/main/java/life/bareun/diary/habit/repository/MemberHabitRepositoryCustomImpl.java
+++ backend/src/main/java/life/bareun/diary/habit/repository/MemberHabitRepositoryCustomImpl.java
@@ -2,8 +2,12 @@ package life.bareun.diary.habit.repository;
 
 import static life.bareun.diary.habit.entity.QMemberHabit.memberHabit;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import life.bareun.diary.member.dto.MemberHabitsDto;
 import life.bareun.diary.habit.dto.MemberHabitModifyDto;
+import life.bareun.diary.member.dto.MemberPracticeCountPerHourDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +22,26 @@ public class MemberHabitRepositoryCustomImpl implements MemberHabitRepositoryCus
         queryFactory.update(memberHabit).set(memberHabit.isDeleted, true)
             .set(memberHabit.succeededDatetime, memberHabitModifyDto.succeededTime())
             .where(memberHabit.id.eq(memberHabitModifyDto.memberHabitId())).execute();
+    }
+
+    @Override
+    public List<MemberHabitsDto> findAllByMemberIdOrderByCreatedDatetime(Long id) {
+        return queryFactory.select(
+                Projections.constructor(
+                    MemberHabitsDto.class,
+                    memberHabit.habit.name.as("name"),
+                    memberHabit.alias.as("alias"),
+                    memberHabit.id.as("memberHabitId"),
+                    memberHabit.icon.as("icon"),
+                    memberHabit.createdDatetime.as("createdAt")
+                )
+            )
+            .from(memberHabit)
+            .where(memberHabit.member.id.eq(id))
+            .orderBy(
+                memberHabit.createdDatetime.desc()
+            )
+            .fetch();
+
     }
 }
