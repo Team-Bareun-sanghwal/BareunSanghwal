7bb4298d90dabf09056cb47c6559e051943bdb42
신종욱 <rmm1@naver.com>
2024-05-05 20:57:38 +0900

    ref: OAuth2Provider에 PROTECTED 추가

--- backend/src/main/java/life/bareun/diary/global/auth/embed/OAuth2Provider.java
+++ backend/src/main/java/life/bareun/diary/global/auth/embed/OAuth2Provider.java
@@ -1,12 +1,22 @@
 package life.bareun.diary.global.auth.embed;
 
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+
 @RequiredArgsConstructor
+@Getter
 public enum OAuth2Provider {
     GOOGLE("GOOGLE"),
-    KAKAO("KAKAO");
+    KAKAO("KAKAO"),
+    PROTECTED("[PROTECTED]"); // 로그인 이후에 사용됨
+    
 
     private final String value;
+
+
+    public static boolean validate(String name) {
+        return name.equals(GOOGLE.value) || name.equals(KAKAO.value);
+    }
 }
