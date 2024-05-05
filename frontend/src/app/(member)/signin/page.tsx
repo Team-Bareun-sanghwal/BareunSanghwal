57f6b6b4fbb7686d21bc690938e65cefaa154b8f
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-06 00:41:48 +0900

    design: add padding

--- frontend/src/app/(member)/signin/page.tsx
+++ frontend/src/app/(member)/signin/page.tsx
@@ -3,7 +3,7 @@ import { SignInForm } from '@/components/member/SignInForm/SignInForm';
 
 export default function Page() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="p-[1rem] flex flex-col h-screen">
       <div className="custom-bold-text text-custom-black h-[6rem] mb-[5rem]">
         <p className="text-custom-matcha">반가워요!</p>
         <p>우리 딱 네 가지만 정하고 시작할까요?</p>
