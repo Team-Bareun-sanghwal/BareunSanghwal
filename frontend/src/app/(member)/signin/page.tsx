38b3fc3d3199141a54f907b80c41252ff8b3cea0
유현지 <ccamy0915@naver.com>
2024-05-07 08:57:08 +0900

    S10P31A106-6 fix: fix ESLint

--- frontend/src/app/(member)/signin/page.tsx
+++ frontend/src/app/(member)/signin/page.tsx
@@ -1,15 +1,16 @@
-import { Button } from '@/components';
 import { SignInForm } from '@/components/member/SignInForm/SignInForm';
 
 export default function Page() {
   return (
-    <div className="p-[1rem] flex flex-col min-h-screen">
-      <div className="custom-bold-text text-custom-black h-[6rem] mb-[5rem]">
-        <p className="text-custom-matcha">반가워요!</p>
-        <p>우리 딱 네 가지만 정하고 시작할까요?</p>
-      </div>
-      <div className="h-full">
-        <SignInForm />
+    <div className="h-screen">
+      <div className="p-[1rem] flex flex-col justify-between min-h-ful">
+        <div className="custom-bold-text text-custom-black h-[6rem] mb-[5rem]">
+          <p className="text-custom-matcha">반가워요!</p>
+          <p>우리 딱 네 가지만 정하고 시작할까요?</p>
+        </div>
+        <div className="bg-custom-pink h-full">
+          <SignInForm />
+        </div>
       </div>
     </div>
   );
