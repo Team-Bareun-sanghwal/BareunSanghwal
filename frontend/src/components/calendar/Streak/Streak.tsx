e11417f7a176916870f53f88343c96cc64c4460a
김형진 <rlagudwls3469@gmail.com>
2024-05-07 14:04:44 +0900

    feat: 스트릭 리커버리 적용

--- frontend/src/components/calendar/Streak/Streak.tsx
+++ frontend/src/components/calendar/Streak/Streak.tsx
@@ -3,7 +3,7 @@ import { StarIcon } from '@heroicons/react/24/solid';
 import { useOverlay } from '@/hooks/use-overlay';
 import { BottomSheet } from '@/components/common/BottomSheet/BottomSheet';
 import { ThemeColor } from '../CalenderConfig';
-import { getToday, getMonth, getYear } from '../util';
+import { getToday, getMonth, getYear, convertMonthFormat } from '../util';
 import { $Fetch } from '@/apis';
 interface StreakProps {
   themeColor: ThemeColor;
@@ -47,30 +47,15 @@ export const Streak = ({
   };
 
   const Recovoery = () => {
-    // Recovery.then((res) => {
-    //   if (res.status === 200) {
-    //     overlay.open(({ isOpen, close }) => (
-    //       <BottomSheet
-    //         description={`${day}일의 스트릭이 복구되었어요!`}
-    //         mode="POSITIVE"
-    //         onClose={close}
-    //         open={isOpen}
-    //         title={'스트릭 복구 성공!'}
-    //       />
-    //     ));
-    //   } else {
-    //     overlay.open(({ isOpen, close }) => (
-    //       <BottomSheet
-    //         description="스트릭 복구권은 매월 한달 무료로 제공되요! 더 필요하면 상점을 이용해주세요!"
-    //         mode="NEGATIVE"
-    //         onClose={close}
-    //         open={isOpen}
-    //         title={'스트릭 복구권이 없어요...'}
-    //       />
-    //     ));
-    //   }
-    // });
-
+    // const MM = month ? parseInt(convertMonthFormat(month)) : 0;
+    // if (MM !== 0) {
+    //   const response = $Fetch({
+    //     method: 'POST',
+    //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/streaks/recovery/${year}}-${MM}}-${day}`,
+    //     cache: 'no-cache',
+    //   });
+    //   console.log(response);
+    // }
     // Sample
     const myRecovoeryCount = 0;
     if (myRecovoeryCount > 0) {
@@ -111,9 +96,7 @@ export const Streak = ({
     }
   }
   const customClassName = getClassName();
-  const onClick = () => {
-    console.log(day);
-  };
+
   console.log(day, month, year, getToday(false), getMonth(false), getYear());
   return (
     <button onClick={() => onClickStreakRecovery()} className={customClassName}>
