3e9afe13e1baabf0d01b2ada4ebc5655be804667
신종욱 <rmm1@naver.com>
2024-05-08 01:15:57 +0900

    style: CheckStyle 적용

--- backend/src/main/java/life/bareun/diary/product/repository/StreakColorRepository.java
+++ backend/src/main/java/life/bareun/diary/product/repository/StreakColorRepository.java
@@ -1,12 +1,14 @@
 package life.bareun.diary.product.repository;
 
 import java.util.List;
+import java.util.Optional;
 import life.bareun.diary.product.entity.StreakColor;
 import life.bareun.diary.product.entity.StreakColorGrade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StreakColorRepository extends JpaRepository<StreakColor, Integer> {
 
-    List<StreakColor> findByStreakColorGrade(StreakColorGrade streakColorGrade);
+    List<StreakColor> findAllByStreakColorGrade(StreakColorGrade gotchaGrade);
+
     Optional<StreakColor> findByName(String name);
 }
