aeb12260b6dcbdeb8f9b5e81a2e3af93f791cd9c
신종욱 <rmm1@naver.com>
2024-05-07 13:24:26 +0900

    fix: 상품 API 로직 오류 수정

--- backend/src/main/java/life/bareun/diary/member/repository/MemberRecoveryRepository.java
+++ backend/src/main/java/life/bareun/diary/member/repository/MemberRecoveryRepository.java
@@ -1,12 +1,13 @@
 package life.bareun.diary.member.repository;
 
 import java.util.Optional;
+import life.bareun.diary.member.entity.Member;
 import life.bareun.diary.member.entity.MemberRecovery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRecoveryRepository extends JpaRepository<MemberRecovery, Long> {
 
-    Optional<MemberRecovery> findByMemberId(Long id);
+    Optional<MemberRecovery> findByMember(Member member);
 
     void deleteByMemberId(Long id);
 }
