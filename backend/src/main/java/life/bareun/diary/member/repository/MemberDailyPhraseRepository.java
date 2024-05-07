98b49a79b3848adb4f3aa91cb3c3d4c9b0cbf327
신종욱 <rmm1@naver.com>
2024-05-07 14:38:35 +0900

    feat: 오늘의 문구 API repository 메소드 추가

--- backend/src/main/java/life/bareun/diary/member/repository/MemberDailyPhraseRepository.java
+++ backend/src/main/java/life/bareun/diary/member/repository/MemberDailyPhraseRepository.java
@@ -1,9 +1,12 @@
 package life.bareun.diary.member.repository;
 
+import java.util.Optional;
+import life.bareun.diary.member.entity.Member;
 import life.bareun.diary.member.entity.MemberDailyPhrase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberDailyPhraseRepository extends JpaRepository<MemberDailyPhrase, Long>,
     MemberDailyPhraseRepositoryCustom {
 
+    Optional<MemberDailyPhrase> findByMember(Member member);
 }
