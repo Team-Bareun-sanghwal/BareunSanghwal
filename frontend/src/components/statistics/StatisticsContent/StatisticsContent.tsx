d59e80dbffebc2ea6f8989c5e2cccb87e81f6ba7
유현지 <ccamy0915@naver.com>
2024-05-07 17:20:20 +0900

    S10P31A106-6 update: 속성 이름 변경

--- frontend/src/components/statistics/StatisticsContent/StatisticsContent.tsx
+++ frontend/src/components/statistics/StatisticsContent/StatisticsContent.tsx
@@ -5,205 +5,28 @@ import { StatisticsSpineAreaGraph } from '../StatisticsSpineAreaGraph/Statistics
 import { ReportOnlyText } from '../ReportOnlyText/ReportOnlyText';
 import { GuideBox } from '@/components/common/GuideBox/GuideBox';
 import { ColoredSentence } from '@/components/common/ColoredSentence/ColoredSentence';
-
-// const jsonAccess = await $Fetch({
-//   method: 'GET',
-//   url: `${process.env.NEXT_PUBLIC_BASE_URL}/members/statistic`,
-//   cache: 'default',
-// });
-
-// console.log(jsonAccess);
-
-const data = {
-  topPracticedHabits: [
-    {
-      habit: '위스키 마시기',
-      value: 27,
-    },
-    {
-      habit: '와인 마시기',
-      value: 21,
-    },
-    {
-      habit: '악기 연주',
-      value: 18,
-    },
-    {
-      habit: '요리하기',
-      value: 15,
-    },
-    {
-      habit: '축구',
-      value: 11,
-    },
-    {
-      habit: '기타',
-      value: 8,
-    },
-  ],
-  maxPracticedHabit: '위스키 마시기',
-
-  practicePerDayOfWeek: [
-    {
-      day: '일',
-      value: 45,
-      colorIdx: 1,
-    },
-    {
-      day: '월',
-      value: 67,
-      colorIdx: 2,
-    },
-    {
-      day: '화',
-      value: 56,
-      colorIdx: 1,
-    },
-    {
-      day: '수',
-      value: 53,
-      colorIdx: 1,
-    },
-    {
-      day: '목',
-      value: 32,
-      colorIdx: 1,
-    },
-    {
-      day: '금',
-      value: 11,
-      colorIdx: 0,
-    },
-    {
-      day: '토',
-      value: 12,
-      colorIdx: 1,
-    },
-  ],
-
-  achievement_per_hour: [
-    {
-      time: 0,
-      value: 34,
-    },
-    {
-      time: 1,
-      value: 8,
-    },
-    {
-      time: 2,
-      value: 2,
-    },
-    {
-      time: 3,
-      value: 5,
-    },
-    {
-      time: 4,
-      value: 5,
-    },
-    {
-      time: 5,
-      value: 3,
-    },
-    {
-      time: 6,
-      value: 14,
-    },
-    {
-      time: 7,
-      value: 37,
-    },
-    {
-      time: 8,
-      value: 26,
-    },
-    {
-      time: 9,
-      value: 17,
-    },
-    {
-      time: 10,
-      value: 15,
-    },
-    {
-      time: 11,
-      value: 28,
-    },
-    {
-      time: 12,
-      value: 73,
-    },
-    {
-      time: 13,
-      value: 83,
-    },
-    {
-      time: 14,
-      value: 75,
-    },
-    {
-      time: 15,
-      value: 77,
-    },
-    {
-      time: 16,
-      value: 80,
-    },
-    {
-      time: 17,
-      value: 86,
-    },
-    {
-      time: 18,
-      value: 92,
-    },
-    {
-      time: 19,
-      value: 96,
-    },
-    {
-      time: 20,
-      value: 71,
-    },
-    {
-      time: 21,
-      value: 73,
-    },
-    {
-      time: 22,
-      value: 35,
-    },
-    {
-      time: 23,
-      value: 22,
-    },
-  ],
-
-  // 5 Figma 수치와 동일하게 설정해둠
-  totalDays: 173,
-  streakDays: 149,
-  starredDays: 52,
-  longestStreak: 103,
-};
+import { getStatisticsData } from '@/app/(statistics)/_apis/getStatisticsData';
 
 let maxDayValue = 0;
 let minDayValue = 999999;
 let maxDay = '';
 let minDay = '';
 
-data.practicePerDayOfWeek.map((day) => {
-  if (maxDayValue < day.value) {
-    maxDayValue = day.value;
-    maxDay = day.day;
-  }
-  if (day.value < minDayValue) {
-    minDayValue = day.value;
-    minDay = day.day;
-  }
-});
+export const StatisticsContent = async () => {
+  const data = await getStatisticsData();
+  console.log(data);
 
-export const StatisticsContent = () => {
+  data.practiceCountsPerDayOfWeek.map((day: { day: string; value: number }) => {
+    if (maxDayValue < day.value) {
+      maxDayValue = day.value;
+      maxDay = day.day;
+    }
+    if (day.value < minDayValue) {
+      minDayValue = day.value;
+      minDay = day.day;
+    }
+  });
+
   return (
     <div className="py-[2rem] flex flex-col gap-[3rem]">
       <ReportOnlyText
@@ -215,7 +38,7 @@ export const StatisticsContent = () => {
 
       <div>
         <p className="custom-medium-text">상위 달성 해빗</p>
-        <StatisticsPieChart data={data.topPracticedHabits} />
+        <StatisticsPieChart data={data.practicedHabitsTop} />
         <GuideBox
           guideText={
             <ColoredSentence
@@ -229,7 +52,7 @@ export const StatisticsContent = () => {
 
       <div className="flex flex-col items-center">
         <p className="custom-medium-text mr-auto">요일 통계</p>
-        <StatisticsBarChart data={data.practicePerDayOfWeek} />
+        <StatisticsBarChart data={data.practiceCountsPerDayOfWeek} />
         <div className="h-[2rem]" />
         <GuideBox
           guideText={
@@ -251,9 +74,9 @@ export const StatisticsContent = () => {
 
       <div>
         <p className="custom-medium-text">해빗 달성 시간</p>
-        <StatisticsSpineAreaGraph data={data.achievement_per_hour} />
+        <StatisticsSpineAreaGraph data={data.practiceCountsPerHour} />
         <div className="h-[2rem]" />
-        <GuideBox guideText="내가 해빗을 달성한 시간을 보여주는 그래프예요" />
+        <GuideBox guideText="내가 해빗을 달성한 시간 분포를 보여주는 그래프예요" />
       </div>
     </div>
   );
