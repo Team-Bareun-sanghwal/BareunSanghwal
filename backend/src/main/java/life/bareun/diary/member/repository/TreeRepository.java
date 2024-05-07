e0c1f69cacc277fae2ed1dcafc6517641388b171
신종욱 <rmm1@naver.com>
2024-05-07 18:32:03 +0900

    feat: 나무 repository 추가

---
+++ backend/src/main/java/life/bareun/diary/member/repository/TreeRepository.java
@@ -0,0 +1,8 @@
+package life.bareun.diary.member.repository;
+
+import life.bareun.diary.member.entity.Tree;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreeRepository extends JpaRepository<Tree, Long> {
+
+}
