fab418832d77a6fac048e627491a431de62e50ea
유현지 <ccamy0915@naver.com>
2024-05-07 14:38:11 +0900

    S10P31A106-6 update: desc 속성 추가

--- frontend/src/components/mypage/InfoModify/InfoModify.stories.ts
+++ frontend/src/components/mypage/InfoModify/InfoModify.stories.ts
@@ -12,6 +12,7 @@ const meta = {
   tags: ['autodocs'],
   argTypes: {
     title: { description: '수정할 정보' },
+    desc: { description: '수정할 정보(한글)' },
     prevData: { description: '기본 값' },
   },
 } satisfies Meta<typeof InfoModify>;
@@ -21,28 +22,32 @@ type Story = StoryObj<typeof meta>;
 
 export const Nickname: Story = {
   args: {
-    title: '닉네임',
+    title: 'nickname',
+    desc: '닉네임',
     prevData: '으앙앙',
   },
 };
 
 export const Sex: Story = {
   args: {
-    title: '성별',
+    title: 'gender',
+    desc: '성별',
     prevData: 'F',
   },
 };
 
 export const Job: Story = {
   args: {
-    title: '직업',
+    title: 'job',
+    desc: '직업',
     prevData: 'JOB_SEEKER',
   },
 };
 
 export const Birthday: Story = {
   args: {
-    title: '생일',
+    title: 'birthDate',
+    desc: '생일',
     prevData: '1999-01-07',
   },
 };
