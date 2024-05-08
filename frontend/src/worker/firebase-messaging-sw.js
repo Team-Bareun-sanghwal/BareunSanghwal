d9f62bd07536e6487aa427e2ce85f868ea68ae8e
유현지 <ccamy0915@naver.com>
2024-05-08 18:13:15 +0900

    S10P31A106-9 feat: Notification 지원 여부 분기 로직 추가

--- frontend/src/worker/firebase-messaging-sw.js
+++ frontend/src/worker/firebase-messaging-sw.js
@@ -16,6 +16,30 @@ export async function requestToken() {
   const app = initializeApp(firebaseConfig);
   const messaging = getMessaging(app);
 
+  if ('Notification' in window) {
+    // Notification API가 지원되는 경우
+    if (Notification.permission === 'default') {
+      // 권한이 설정되지 않은 경우, 사용자에게 권한 요청
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          console.log('Notification permission granted.');
+        } else {
+          console.log('Notification permission denied.');
+          return;
+        }
+      });
+    } else if (Notification.permission === 'granted') {
+      console.log('Notification permission already granted.');
+    } else {
+      console.log('Notification permission denied.');
+      return;
+    }
+  } else {
+    // Notification API가 지원되지 않는 경우
+    console.log('This browser does not support notifications.');
+    return;
+  }
+
   const token = await getToken(messaging, {
     vapidKey: process.env.NEXT_PUBLIC_VAPID_KEY,
   });
