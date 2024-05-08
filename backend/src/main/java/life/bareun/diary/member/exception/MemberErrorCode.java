a574ea19ae0c30a829fbba0b1754b4a9a8e0ace4
신종욱 <rmm1@naver.com>
2024-05-08 16:37:10 +0900

    feat: 스트릭 리커버리 사용 불가 에러 코드 추가

--- backend/src/main/java/life/bareun/diary/member/exception/MemberErrorCode.java
+++ backend/src/main/java/life/bareun/diary/member/exception/MemberErrorCode.java
@@ -14,7 +14,8 @@ public enum MemberErrorCode {
     NO_INITIAL_DATA_STREAK_COLOR(HttpStatus.NOT_FOUND, "회원가입을 위한 초기 스트릭 색상 데이터가 없습니다."),
     NO_INITIAL_DATA_TREE_COLOR(HttpStatus.NOT_FOUND, "회원가입을 위한 초기 나무 색상 데이터가 없습니다."),
     ALREADY_HARVESTED(HttpStatus.FORBIDDEN, "오늘은 이미 나무 포인트를 수확했습니다."),
-    NO_SUCH_DAILY_PHRASE(HttpStatus.NOT_FOUND, "존재하지 않는 오늘의 문구의 정보입니다.");
+    NO_SUCH_DAILY_PHRASE(HttpStatus.NOT_FOUND, "존재하지 않는 오늘의 문구의 정보입니다."),
+    STREAK_RECOVERY_UNAVAILABLE(HttpStatus.UNPROCESSABLE_ENTITY, "보유한 스트릭 리커버리가 없습니다");
 
 
     private final HttpStatus status;
