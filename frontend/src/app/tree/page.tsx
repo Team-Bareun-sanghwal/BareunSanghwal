b33a5ec77a82d08e83aacc9e8bf183585fd7fe2f
김형진 <rlagudwls3469@gmail.com>
2024-05-08 12:34:20 +0900

    feat: point 컴포넌트

--- frontend/src/app/tree/page.tsx
+++ frontend/src/app/tree/page.tsx
@@ -1,13 +1,8 @@
-'use client';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
-import lottieJson from '@/../public/lotties/lottie-lego.json';
+// import { useRouter } from 'next/navigation';
 import Item from '@/components/point/Item/Item';
 import { MyPoint } from '@/components/point/MyPoint/MyPoint';
 import { $Fetch } from '@/apis';
-const LottieBox = dynamic(() => import('react-lottie-player'), { ssr: false });
-
+import Tree from '@/components/tree/Tree';
 interface IItem {
   key: string;
   name: string;
@@ -20,78 +15,46 @@ interface IItemList {
   products: IItem[];
 }
 
-export default function Page() {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-  const [showLoader, setShowLoader] = useState(false);
-  const [ItemListResponse, setItemListResponse] = useState<IItemList>({
-    products: [],
+export default async function Page() {
+  // const router = useRouter();
+
+  const response = await $Fetch({
+    method: 'GET',
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/products`,
+    cache: 'no-cache',
   });
 
-  // 로딩 지연을 처리하는 useEffect
-  useEffect(() => {
-    if (isLoading) {
-      const timer = setTimeout(() => {
-        setShowLoader(false);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }
-  }, [isLoading]);
-
-  useEffect(() => {
-    $Fetch({
-      method: 'GET',
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/products`,
-      cache: 'no-cache',
-    })
-      .then((data) => {
-        setItemListResponse(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error('ERR:', error);
-        setIsLoading(false);
-      });
-  }, []);
-
-  const exit = () => {
-    setShowLoader(true);
-    router.back();
-  };
+  console.log(response.data);
+  // const exit = () => {
+  //   router.back();
+  // };
 
   return (
     <div>
-      {showLoader ? (
-        <div className="flex flex-col w-full h-screen justify-center items-center content-center">
-          <LottieBox
-            loop
-            animationData={lottieJson}
-            play
-            className="w-[10rem] h-[10rem]"
-          />
-        </div>
-      ) : (
-        <div className="w-full h-screen overflow-hidden relative">
-          <button onClick={exit} className="absolute z-10 m-4 text-lg">
-            {'<'} 뒤로가기
-          </button>
-          <div className="absolute bottom-0 w-full gap-3 p-3">
-            <div className="flex flex-col justify-center gap-4">
-              <MyPoint />
-              {ItemListResponse.products.map((item) => (
-                <Item
-                  key={item.key}
-                  keyname={item.key}
-                  name={item.name}
-                  introduction={item.introduction}
-                  description={item.description}
-                  price={item.price}
-                />
-              ))}
-            </div>
+      <div className="w-full h-screen overflow-hidden relative">
+        <button
+          // onClick={exit}
+          className="absolute z-10 m-4 text-lg"
+        >
+          {'<'} 뒤로가기
+        </button>
+        <Tree color="red" />
+        <div className="absolute bottom-0 w-full gap-3 p-3">
+          <div className="flex flex-col justify-center gap-4">
+            <MyPoint />
+            {response?.data.products.map((item: IItem) => (
+              <Item
+                key={item.key}
+                keyname={item.key}
+                name={item.name}
+                introduction={item.introduction}
+                description={item.description}
+                price={item.price}
+              />
+            ))}
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- frontend/src/components/point/MyPoint/MyPoint.tsx
+++ frontend/src/components/point/MyPoint/MyPoint.tsx
@@ -1,10 +1,11 @@
 import { $Fetch } from '@/apis';
 import Point from '../Point/Point';
 export const MyPoint = async () => {
-  const { point } = await $Fetch({
+  const response = await $Fetch({
     method: 'GET',
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/members/point`,
     cache: 'no-cache',
   });
-  return <Point point={point} />;
+
+  return <Point point={response.data.point} />;
 };
