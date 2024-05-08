31c29cb5bdd31d66cc9ad1ec5d1335fe6ee93d8e
전영빈 <binkorea2@gmail.com>
2024-05-08 15:46:07 +0900

    S10P31A106-416 ref: 코드 가독성을 높이기 위한 줄바꿈

--- backend/src/main/java/life/bareun/diary/streak/dto/StreakInfoByDayDto.java
+++ backend/src/main/java/life/bareun/diary/streak/dto/StreakInfoByDayDto.java
@@ -3,10 +3,9 @@ package life.bareun.diary.streak.dto;
 import lombok.Builder;
 
 @Builder
-/*
- * TODO: 전영빈 // 원래 필드가 int, int, int 였는데 레포지토리에서 findStreakInfo로 받은 반환 값이
- *  Integer, Long, Long 이었다. 이유는 아직 모르고 알아봐야 한다.
- */
-public record StreakInfoByDayDto(Integer dayNumber, Long achieveCount, Long totalCount) {
+public record StreakInfoByDayDto(
+    Integer dayNumber,
+    Long achieveCount,
+    Long totalCount) {
 
 }
--- backend/src/main/java/life/bareun/diary/streak/dto/response/MemberStreakResDto.java
+++ backend/src/main/java/life/bareun/diary/streak/dto/response/MemberStreakResDto.java
@@ -3,7 +3,10 @@ package life.bareun.diary.streak.dto.response;
 import lombok.Builder;
 
 @Builder
-public record MemberStreakResDto(int totalStreakCount, int achieveStreakCount, int starCount,
-                                 int longestStreakCount) {
+public record MemberStreakResDto(
+    int totalStreakCount,
+    int achieveStreakCount,
+    int starCount,
+    int longestStreakCount) {
 
 }
