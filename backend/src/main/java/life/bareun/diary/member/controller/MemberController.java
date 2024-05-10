d0f50cd0ff998989f647a32938f5fd443ead2a76
신종욱 <rmm1@naver.com>
2024-05-10 14:23:00 +0900

    ref: 스트릭 정보 API 요청 경로 수정

--- backend/src/main/java/life/bareun/diary/member/controller/MemberController.java
+++ backend/src/main/java/life/bareun/diary/member/controller/MemberController.java
@@ -10,7 +10,7 @@ import life.bareun.diary.member.dto.response.MemberInfoResDto;
 import life.bareun.diary.member.dto.response.MemberLongestStreakResDto;
 import life.bareun.diary.member.dto.response.MemberPointResDto;
 import life.bareun.diary.member.dto.response.MemberStatisticResDto;
-import life.bareun.diary.member.dto.response.MemberStreakColorResDto;
+import life.bareun.diary.member.dto.response.MemberStreakInfoResDto;
 import life.bareun.diary.member.dto.response.MemberStreakRecoveryCountResDto;
 import life.bareun.diary.member.dto.response.MemberTreeColorResDto;
 import life.bareun.diary.member.dto.response.MemberTreePointResDto;
@@ -108,9 +108,9 @@ public class MemberController {
             );
     }
 
-    @GetMapping("/color/streak")
-    public ResponseEntity<BaseResponse<MemberStreakColorResDto>> streakColor() {
-        MemberStreakColorResDto memberStreakColorResDto = memberService.streakColor();
+    @GetMapping("/streak")
+    public ResponseEntity<BaseResponse<MemberStreakInfoResDto>> streakColor() {
+        MemberStreakInfoResDto memberStreakInfoResDto = memberService.streakInfo();
         return ResponseEntity
             .status(
                 HttpStatus.OK.value()
