e910d71f34936d95e5654a8bc44ec413511a6d7d
유현지 <ccamy0915@naver.com>
2024-05-10 14:35:50 +0900

    S10P31A106-9 fix: 백그라운드에서 알림 두 번 전송되는 에러 해결

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,40 @@
+importScripts(
+  'https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js',
+);
+importScripts(
+  'https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js',
+);
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAbcSOk4v64N_OtDrAwg6NqCfD4V9ybRVM',
+  authDomain: 'bareun-life.firebaseapp.com',
+  projectId: 'bareun-life',
+  storageBucket: 'bareun-life.appspot.com',
+  messagingSenderId: '440103641572',
+  appId: '1:440103641572:web:5da3f683fe540b222827f1',
+});
+
+const messaging = firebase.messaging();
+
+// push 발생 시 알림 띄우기
+self.addEventListener('push', function (e) {
+  if (!e.data.json()) return;
+
+  const resultData = e.data.json();
+
+  console.log(resultData);
+
+  const notificationTitle = resultData.data.title;
+  const notificationOptions = {
+    body: resultData.data.body,
+    data: resultData.data,
+    ...resultData,
+  };
+  registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// 알림 클릭 시 알림 페이지로 이동
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
