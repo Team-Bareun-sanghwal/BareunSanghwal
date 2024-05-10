2af2d604f6e1fc06847bc7408560594e40b6bb5a
유현지 <ccamy0915@naver.com>
2024-05-10 14:37:29 +0900

    S10P31A106-9 design: 열람/미열람 메세지 구분, 뒤로가기 누를 시 전제 열람 처리 design: 알림 한 페이지 이하일 경우 레이아웃 조정

--- frontend/src/app/notification/page.tsx
+++ frontend/src/app/notification/page.tsx
@@ -1,39 +1,15 @@
-import { getNotificationList } from './_apis/getNotificationList';
-import { SingleNotification } from './../../components/notification/SingleNotification/SingleNotification';
 import { BackToHomeButton } from '@/components/notification/BackToHomeButton/BackToHomeButton';
-
-interface INotificationType {
-  notificationId: number;
-  icon: string;
-  content: string;
-  isRead: boolean;
-  createdAt: string;
-}
+import { NotificationList } from '@/components/notification/NotificationList/NotificationList';
 
 export default async function Page() {
-  const result = await getNotificationList();
-  const notificationList = result.notificationList;
-
   return (
     <div className="bg-custom-white p-[1rem] flex flex-col justify-between min-h-full">
-      <nav className="flex self-start gap-[0.5rem] items-center">
+      <nav className="flex self-start gap-[0.5rem] items-center w-full h-fit">
         <BackToHomeButton />
-        <span className="custom-bold-text">신규 해빗 등록</span>
+        <span className="custom-bold-text">알림</span>
       </nav>
-      <div className="flex flex-col items-center mt-[1rem]">
-        {notificationList.map((no: INotificationType, index: number) => {
-          return (
-            <SingleNotification
-              key={no.notificationId}
-              index={index}
-              notificationId={no.notificationId}
-              icon={no.icon}
-              content={no.content}
-              isRead={no.isRead}
-              createdAt={no.createdAt}
-            />
-          );
-        })}
+      <div className="mt-[1rem] mb-auto">
+        <NotificationList />
       </div>
     </div>
   );
