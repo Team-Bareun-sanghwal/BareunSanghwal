fe58418ad9acef620b3af251995cb7f8154c5c5a
jsm0726 <jsm0726@naver.com>
2024-05-13 15:14:58 +0900

    S10P31A106-2 feat: 추천 해빗 카테고리 10개를 다시 불러오는 클릭 이벤트 핸들링

--- frontend/src/app/habit/_apis/getSimilarCategoryList.ts
+++ frontend/src/app/habit/_apis/getSimilarCategoryList.ts
@@ -3,11 +3,10 @@ import { $Fetch } from '@/apis';
 export async function getSimilarCategoryList() {
   const response = await $Fetch({
     method: 'GET',
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/members/recommendation/habits`,
+    url: `https://bareun.life/recommendation/habits`,
     cache: 'no-store',
   });
 
-  if (response.status !== 200)
-    throw new Error('추천 습관 10가지를 불러오는데 실패');
+  if (!response.ok) throw new Error('추천 습관 10가지를 불러오는데 실패');
   return response;
 }
