120dd2f4654eadfb68725b6e1fded237e217b20a
유현지 <ccamy0915@naver.com>
2024-05-13 10:53:04 +0900

    S10P31A106-9 design: 회원탈퇴/로그아웃 버튼 위치 조정

--- frontend/src/app/(member)/mypage/page.tsx
+++ frontend/src/app/(member)/mypage/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
   return (
     <div className="h-screen">
       <div className="bg-custom-white px-[1rem] pt-[1rem] pb-[11rem] flex flex-col justify-between min-h-full">
-        <div>
+        <div className="pb-[2rem]">
           <p className="text-custom-black custom-bold-text mb-[4rem]">
             내 정보
           </p>
@@ -22,7 +22,7 @@ export default async function Page() {
             }}
           />
         </div>
-        <div className="flex justify-evenly mt-[3rem]">
+        <div className="flex justify-evenly">
           <BorderlessButton type="leave" />
           <span className="bg-custom-medium-gray w-[0.1rem]"></span>
           <BorderlessButton type="logout" />
