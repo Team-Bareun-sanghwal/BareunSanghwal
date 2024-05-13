e4255bc7726baf807748daaf31746ee8d246d206
신종욱 <rmm1@naver.com>
2024-05-13 14:34:20 +0900

    ref: 리프레시 토큰 만료 응답 코드 수정

--- backend/src/main/java/life/bareun/diary/global/auth/exception/SecurityErrorCode.java
+++ backend/src/main/java/life/bareun/diary/global/auth/exception/SecurityErrorCode.java
@@ -12,7 +12,7 @@ public enum SecurityErrorCode {
     UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
     AUTHENTICATION_FAILURE(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다."),
-    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "다시 로그인하십시오."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "다시 로그인하십시오."),
     INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "인증 정보가 유효하지 않습니다."),
     NO_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰 발급을 위한 정보가 없습니다"),
     UNMATCHED_AUTHENTICATION(HttpStatus.BAD_REQUEST, "인증 정보가 일치하지 않습니다."),
