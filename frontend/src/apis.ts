d20944de10d6fb61cb705bf9c4d1fad76aa61c93
유현지 <ccamy0915@naver.com>
2024-05-14 10:50:17 +0900

    S10P31A106-507 feat: refresh token 적용

--- frontend/src/apis.ts
+++ frontend/src/apis.ts
@@ -14,22 +14,116 @@ type Request = {
     | 'only-if-cached'
     | 'default';
 };
+
+// export async function $SetCookie({ at, rt }: { at: string; rt: string }) {
+//   const cookieStore = cookies();
+//   cookieStore.set('Authorization', at, { maxAge: 10 });
+//   cookieStore.set('RefreshToken', rt);
+// }
+
 export async function $Fetch({ method, url, data, cache }: Request) {
   const cookieStore = cookies();
   // const authorization = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
   const authorization = cookieStore.get('Authorization')?.value;
   const refreshToken = cookieStore.get('RefreshToken')?.value;
+  console.log('authorization: ', authorization);
+  console.log('refreshToken: ', refreshToken);
+
+  if (authorization !== undefined) {
+    try {
+      const res = await fetch(url, {
+        method,
+        cache: cache,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: authorization as string,
+        },
+        body: JSON.stringify(data),
+      });
+
+      const json = await res.json();
+      switch ((await json).status) {
+        case 200:
+          console.log('정상처리');
+          return await json;
+        case 500:
+          console.log('서버 오류 발생');
+          break;
+      }
+    } catch (e) {
+      console.log('Fetch Error : ', e);
+      throw e;
+    }
+  } else {
+    console.log('Access Token 만료');
+
+    const result = await $GetRefreshToken();
+    if (result.status === 200) {
+      console.log('Access Token 재발급 성공');
+
+      // await $Fetch({ method, url, data, cache });
+    } else {
+      console.log('Access Token 재발급 실패');
+      // window.location.href = '/';
+    }
+  }
+}
+
+export async function $GetRefreshToken() {
+  const cookieStore = cookies();
+  // const refreshToken = process.env.NEXT_PUBLIC_REFRESH_TOKEN;
+  const refreshToken = cookieStore.get('RefreshToken')?.value;
+  console.log('재발급 받을 refresh token은 ', refreshToken);
 
   try {
-    const res = await fetch(url, {
-      method,
-      cache: cache,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: authorization as string,
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/auth/access-token`,
+      {
+        method: 'GET',
+        cache: 'default',
+        headers: {
+          'Content-Type': 'application/json',
+          RefreshToken: refreshToken as string,
+        },
       },
-      body: JSON.stringify(data),
-    });
+    );
+    console.log('새 토큰');
+    // console.log((await res.json()).data);
+    // console.log(res.json());
+
+    return await res.json();
+  } catch (e) {
+    console.log('Fetch Error : ', e);
+    throw e;
+  }
+}
+
+export async function $Logout() {
+  const cookieStore = cookies();
+  // const authorization = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+  // const refreshToken = process.env.NEXT_PUBLIC_REFRESH_TOKEN;
+  const authorization = cookieStore.get('Authorization')?.value;
+  const refreshToken = cookieStore.get('RefreshToken')?.value;
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/members/logout`,
+      {
+        method: 'POST',
+        cache: 'default',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: authorization as string,
+          RefreshToken: refreshToken as string,
+        },
+      },
+    );
+
+    if ((await res.json()).status === 200) {
+      cookieStore.delete('Authorization');
+      cookieStore.delete('RefreshToken');
+      window.location.href = '/';
+    }
 
     return await res.json();
   } catch (e) {
