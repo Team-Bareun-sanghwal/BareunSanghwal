044f7cfa03240568ae23360c3bee6d2d545147aa
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-15 16:16:00 +0900

    feat: RefreshToken으로 AccessToken 재발급

--- frontend/src/app/token/page.tsx
+++ frontend/src/app/token/page.tsx
@@ -1,24 +1,15 @@
 'use client';
 
 import { useEffect } from 'react';
-import { $SetCookie, $GetRefreshToken } from '@/apis';
 import { useRouter } from 'next/navigation';
+import { resetAccessToken } from './_apis/resetAccessToken';
 
 export default function Page() {
   const router = useRouter();
 
-  const getRefreshToken = async () => {
-    const res = await $GetRefreshToken();
-    console.log(res);
-
-    if (res.status === 200) {
-      await $SetCookie({ at: res.data });
-      // router.refresh();
-      router.back();
-    }
-  };
-
   useEffect(() => {
-    getRefreshToken();
+    resetAccessToken();
   }, []);
+
+  router.push(sessionStorage.getItem('previousUrl')!);
 }
