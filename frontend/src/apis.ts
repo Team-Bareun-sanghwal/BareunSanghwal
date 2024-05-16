866ad9cc8e3b627cd7c826ca53265bd4561a82db
김형진 <rlagudwls3469@gmail.com>
2024-05-16 16:08:44 +0900

    fix: api.ts

--- frontend/src/apis.ts
+++ frontend/src/apis.ts
@@ -18,57 +18,43 @@ type Request = {
 
 export async function $Fetch({ method, url, data, cache }: Request) {
   const cookieStore = cookies();
-  // const authorization = cookieStore.get('Authorization')?.value;
-  const authorization = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+  const authorization = cookieStore.get('Authorization')?.value;
   const refreshToken = cookieStore.get('RefreshToken')?.value;
 
-  const res = await fetch(url, {
-    method,
-    cache: cache,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: authorization as string,
-      RefreshToken: refreshToken as string,
-    },
-    body: JSON.stringify(data),
-  });
+  if (authorization !== undefined) {
+    try {
+      const res = await fetch(url, {
+        method,
+        cache: cache,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: authorization as string,
+          RefreshToken: refreshToken as string,
+        },
+        body: JSON.stringify(data),
+      });
 
-  // const json = await res.json();
-  return res.json();
-  // if (authorization !== undefined) {
-  //   try {
-  //     const res = await fetch(url, {
-  //       method,
-  //       cache: cache,
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: authorization as string,
-  //         RefreshToken: refreshToken as string,
-  //       },
-  //       body: JSON.stringify(data),
-  //     });
-
-  //     const json = await res.json();
-  //     switch (json.status) {
-  //       case 200:
-  //         if (url === `${process.env.NEXT_PUBLIC_BASE_URL}/members/logout`) {
-  //           cookieStore.delete('Authorization');
-  //           cookieStore.delete('RefreshToken');
-  //           redirect('/');
-  //         }
-  //         break;
-  //       case 500:
-  //         console.log(url, ' 서버 오류 발생');
-  //         break;
-  //     }
-  //     return json;
-  //   } catch (e) {
-  //     console.log('Fetch Error : ', e);
-  //     throw e;
-  //   }
-  // } else {
-  //   redirect(`/token`, RedirectType.replace);
-  // }
+      const json = await res.json();
+      switch (json.status) {
+        case 200:
+          if (url === `${process.env.NEXT_PUBLIC_BASE_URL}/members/logout`) {
+            cookieStore.delete('Authorization');
+            cookieStore.delete('RefreshToken');
+            redirect('/');
+          }
+          break;
+        case 500:
+          console.log(url, ' 서버 오류 발생');
+          break;
+      }
+      return json;
+    } catch (e) {
+      console.log('Fetch Error : ', e);
+      throw e;
+    }
+  } else {
+    redirect(`/token`, RedirectType.replace);
+  }
 }
 
 export async function $SetCookie({ at, rt }: { at: string; rt?: string }) {
