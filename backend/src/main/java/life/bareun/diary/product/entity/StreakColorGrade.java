d477da3ca580f3b8973fc6f4135763ae9c376d7e
신종욱 <rmm1@naver.com>
2024-05-18 23:52:15 +0900

    ref: 스트릭 색상 엔티티 생성자 접근 제한 레벨 수정

--- backend/src/main/java/life/bareun/diary/product/entity/StreakColorGrade.java
+++ backend/src/main/java/life/bareun/diary/product/entity/StreakColorGrade.java
@@ -6,13 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "streak_color_grade")
 public class StreakColorGrade {
