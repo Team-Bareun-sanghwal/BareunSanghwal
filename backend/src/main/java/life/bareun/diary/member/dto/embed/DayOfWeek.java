3e2bb47b6a9f744364830d7a969881386161a8ce
신종욱 <rmm1@naver.com>
2024-05-20 00:53:51 +0900

    ref: 불필요한 메소드 삭제

--- backend/src/main/java/life/bareun/diary/member/dto/embed/DayOfWeek.java
+++ backend/src/main/java/life/bareun/diary/member/dto/embed/DayOfWeek.java
@@ -17,21 +17,4 @@ public enum DayOfWeek {
 
     private final String value;
     private final int index;
-
-    public static String getValueByIndex(int index) {
-        rangeCheck(index);
-        for (DayOfWeek day : DayOfWeek.values()) {
-            if (day.index == index) {
-                return day.getValue();
-            }
-        }
-        // 모든 enum을 확인했음에도 불구하고 해당 index가 없는 경우, 오류를 처리합니다.
-        throw new IllegalArgumentException("Invalid index: " + index);
-    }
-
-    private static void rangeCheck(int index) {
-        if ((index < MON.index) || (SUN.index < index)) {
-            throw new IllegalArgumentException("Index out of range: " + index);
-        }
-    }
 }
