66adaa26709c23c3b586a18b2b78e3ea9a34b9bf
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-21 00:09:51 +0900

    design: 나무 페이지 전체화면 수정

--- frontend/src/app/tree/page.tsx
+++ frontend/src/app/tree/page.tsx
@@ -1,4 +1,3 @@
-
 import { $Fetch } from '@/apis';
 import Tree from '@/components/tree/Tree';
 import { RouteHome } from '@/components/tree/Button/RouteHome/RouteHome';
@@ -33,39 +32,53 @@ export default async function Page() {
   const { treeLevel, treeColor } = treeInfo.data;
   const { point, isHarvestedToday } = pointInfo.data;
 
-  const getTreeColor = (treeColor : string) => {
+  const getTreeColor = (treeColor: string) => {
     switch (treeColor) {
-      case 'red' : return '#ff0000';
-      case ' green' : return '#008000';
-      case 'blue' : return '#0000ff';
-      case 'yellow' : return '#ffff00';
-      case 'orange' : return '#ffa500';
-      case 'purple' : return '#800080';
-      case 'gold' : return '#ffd700';
-      case 'silver' : return '#c0c0c0';
-      case 'cotton_candy' : return '#aee5ff';
-      case 'cherry_blossom' : return '#ffc0cb';
-      default : return '#008000';
+      case 'red':
+        return '#ff0000';
+      case ' green':
+        return '#008000';
+      case 'blue':
+        return '#0000ff';
+      case 'yellow':
+        return '#ffff00';
+      case 'orange':
+        return '#ffa500';
+      case 'purple':
+        return '#800080';
+      case 'gold':
+        return '#ffd700';
+      case 'silver':
+        return '#c0c0c0';
+      case 'cotton_candy':
+        return '#aee5ff';
+      case 'cherry_blossom':
+        return '#ffc0cb';
+      default:
+        return '#008000';
     }
-  }
+  };
   return (
-    <div>
-      <div className="w-full h-screen overflow-hidden relative">
-        <RouteHome />
-        <div className="flex flex-col absolute z-10 top-10 w-full items-center ">
-          <div className=" bg-custom-dark-gray p-4 rounded-md">
-            <div className="text-gray-300 text-md text-center">
-              LEVEL {treeLevel}
-            </div>
-            <div className="text-custom-white text-xl text-center">
-              {treeConfig[treeLevel - 1].name}
-            </div>
+    <div className="w-full min-h-screen overflow-hidden relative">
+      <RouteHome />
+      <div className="flex flex-col absolute z-10 top-10 w-full items-center ">
+        <div className=" bg-custom-dark-gray p-4 rounded-md">
+          <div className="text-gray-300 text-md text-center">
+            LEVEL {treeLevel}
+          </div>
+          <div className="text-custom-white text-xl text-center">
+            {treeConfig[treeLevel - 1].name}
           </div>
         </div>
-        {!isHarvestedToday ? <Harvest isHarvested={false}/>  : <MyPoint />}
-        
-        <Tree color={getTreeColor(treeColor)} level={treeLevel} time={Time()} ItemList={response.data.products} />
       </div>
+      {!isHarvestedToday ? <Harvest isHarvested={false} /> : <MyPoint />}
+
+      <Tree
+        color={getTreeColor(treeColor)}
+        level={treeLevel}
+        time={Time()}
+        ItemList={response.data.products}
+      />
     </div>
   );
 }
